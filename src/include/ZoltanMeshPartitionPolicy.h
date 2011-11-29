b8870134bae816931b82e6e9d0dc8b32333c07a9
jchan985 <jchan985@36274a68-8941-4c93-80ef-affaed5139da>
2011-11-29 17:45:53 +0000

    adding changes to ZoltanMeshPartitionPolicy to repo.  broken at the moment
    git-svn-id: https://svn.ices.utexas.edu/repos/camellia@42 36274a68-8941-4c93-80ef-affaed5139da

--- src/include/ZoltanMeshPartitionPolicy.h
+++ src/include/ZoltanMeshPartitionPolicy.h
@@ -18,17 +18,17 @@ using namespace std;
 
 class ZoltanMeshPartitionPolicy : public MeshPartitionPolicy {
  private: 
-  string _ZoltanPartitioner;
- public:  
-  ZoltanMeshPartitionPolicy(string partitionerName);
-  virtual void partitionMesh(Mesh *mesh, int numPartitions, FieldContainer<int> &partitionedActiveCells);
-  
+  string _ZoltanPartitioner; // default to block
+
+  //helper functions for query functions
+  int getNextActiveIndex(FieldContainer<int> &partitionedActiveCells);
+  int getIndexOfGID(FieldContainer<int> &partitionedActiveCells,int globalID);
+
   //Zoltan query functions
   static int get_number_of_objects(void *data, int *ierr);
   static void get_object_list(void *data, int sizeGID, int sizeLID,ZOLTAN_ID_PTR globalID, ZOLTAN_ID_PTR localID,int wgt_dim, float *obj_wgts, int *ierr);
   static int get_num_geom(void *data, int *ierr);
   static void get_geom_list(void *data,int num_gid_entries, int num_lid_entries, int num_obj,ZOLTAN_ID_PTR global_ids, ZOLTAN_ID_PTR local_ids, int num_dim, double *geom_vec, int *ierr);  
-
 
   //Reftree refinement pattern query functions
   static int get_num_coarse_elem(void *data, int *ierr);
@@ -36,6 +36,13 @@ class ZoltanMeshPartitionPolicy : public MeshPartitionPolicy {
   static int get_num_children(void *data, int num_gid_entries, int num_lid_entries, ZOLTAN_ID_PTR global_id, ZOLTAN_ID_PTR local_id, int *ierr);
   static void get_children(void *data, int num_gid_entries, int num_lid_entries, ZOLTAN_ID_PTR parent_gid, ZOLTAN_ID_PTR parent_lid, ZOLTAN_ID_PTR child_gids, ZOLTAN_ID_PTR child_lids, int *assigned, int *num_vert, ZOLTAN_ID_PTR vertices, ZOLTAN_REF_TYPE *ref_type, ZOLTAN_ID_PTR in_vertex, ZOLTAN_ID_PTR out_vertex, int *ierr);
   static void get_child_weight(void *data, int num_gid_entries, int num_lid_entries, ZOLTAN_ID_PTR global_id, ZOLTAN_ID_PTR local_id, int wgt_dim, float *obj_wgt, int *ierr);  
+
+
+ public:  
+  ZoltanMeshPartitionPolicy();
+  ZoltanMeshPartitionPolicy(string partitionerName);
+  //ZoltanMeshPartitionPolicy();
+  virtual void partitionMesh(Mesh *mesh, int numPartitions, FieldContainer<int> &partitionedActiveCells);  
 
 };
 
