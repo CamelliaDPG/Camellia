bea77891a5adde1f1c6a385dc9c6ae89ca51c468
jchan985 <jchan985@36274a68-8941-4c93-80ef-affaed5139da>
2012-01-13 23:30:34 +0000

    committing changes to Confusion (current driver uses automatic refinement according to energy error, brings up parity error in mesh somehow)
    git-svn-id: https://svn.ices.utexas.edu/repos/camellia@127 36274a68-8941-4c93-80ef-affaed5139da

--- Drivers/Confusion/AdaptiveConfusion.cpp
+++ Drivers/Confusion/AdaptiveConfusion.cpp
@@ -5,8 +5,8 @@
 #include "OptimalInnerProduct.h"
 #include "Mesh.h"
 #include "Solution.h"
-
 #include "ZoltanMeshPartitionPolicy.h"
+#include "MeshTestSuite.h"
 
 // Trilinos includes
 #include "Epetra_Time.h"
@@ -41,18 +41,20 @@ int main(int argc, char *argv[]) {
   int rank = 0;
   int numProcs = 1;
 #endif
-  int polyOrder = 2;
-  int pToAdd = 2; // for tests
+  int polyOrder = 3;
+  int pToAdd = 4; // for tests
   
   // define our manufactured solution:
-  double epsilon = 5e-2;
-  double beta_x = 1.0, beta_y = 2.0;
+  double epsilon = 1e-2;
+  double beta_x = 1.0, beta_y = 1.5;
   bool useTriangles = false;
   bool useEggerSchoeberl = false;
   ConfusionManufacturedSolution exactSolution(epsilon,beta_x,beta_y);
   
   // define our inner product:
   Teuchos::RCP<DPGInnerProduct> ip = Teuchos::rcp( new OptimalInnerProduct( exactSolution.bilinearForm() ) );
+
+  exactSolution.bilinearForm()->printTrialTestInteractions();
   
   FieldContainer<double> quadPoints(4,2);
   
@@ -66,12 +68,13 @@ int main(int argc, char *argv[]) {
   quadPoints(3,1) = 1.0;  
   
   int H1Order = polyOrder + 1;
-  int horizontalCells = 2, verticalCells = 2;
+  int horizontalCells = 2, verticalCells = 2;  
   // create a pointer to a new mesh:
   Teuchos::RCP<Mesh> mesh = Mesh::buildQuadMesh(quadPoints, horizontalCells, verticalCells, exactSolution.bilinearForm(), H1Order, H1Order+pToAdd, useTriangles);
+  cout << "In driver, setting numProcs = " << numProcs << endl;
   mesh->setPartitionPolicy(Teuchos::rcp(new ZoltanMeshPartitionPolicy("HSFC")));
-//  mesh->setPartitionPolicy(Teuchos::rcp(new MeshPartitionPolicy()));
   mesh->setNumPartitions(numProcs);
+  mesh->rebuildLookups();
 
   // create a solution object
   Teuchos::RCP<Solution> solution;
@@ -81,62 +84,113 @@ int main(int argc, char *argv[]) {
     Teuchos::RCP<ConfusionProblem> problem = Teuchos::rcp( new ConfusionProblem() );
     solution = Teuchos::rcp(new Solution(mesh, problem, problem, ip));
   }
-//  vector<int> cellsToRef;
-//  cellsToRef.push_back(0);
-//  mesh->hRefine(cellsToRef,RefinementPattern::regularRefinementPatternQuad());
+ 
+  if (false){
+    // ---------------------- TEST PARITY ----------------------------
+    
+    int numInitialElems = mesh->activeElements().size();
+    vector<int> cells;  
+    cells.push_back(0);
+    cells.push_back(1);
 
-  // solve
-  solution->solve(false); 
+    mesh->hRefine(cells,RefinementPattern::regularRefinementPatternQuad());
+    cout << "mesh test suite consistency parity test returns (1st set of refs) " << MeshTestSuite::checkMeshConsistency(*mesh) << endl;
+    cells.clear();
+    
+    cells.push_back(numInitialElems);  
+    cells.push_back(numInitialElems+1);  
+    cells.push_back(numInitialElems+2);  
+    cells.push_back(numInitialElems+3);  
+    mesh->hRefine(cells,RefinementPattern::regularRefinementPatternQuad());
+    cout << "mesh test suite consistency parity test returns (2nd set of refs) " << MeshTestSuite::checkMeshConsistency(*mesh) << endl;
 
-  cout << "Processor " << rank << " returned from solve()." << endl;
-  
-  if (rank == 0) {
-    // save a data file for plotting in MATLAB
-    solution->writeToFile(ConfusionBilinearForm::U, "Confusion_u_adaptive.dat");
-    solution->writeFluxesToFile(ConfusionBilinearForm::U_HAT, "Confusion_u_hat_adaptive.dat");
-    solution->writeFluxesToFile(ConfusionBilinearForm::BETA_N_U_MINUS_SIGMA_HAT, "Confusion_beta_n_hat_adaptive.dat");
+    // solve
+    solution->solve(false); 
 
-    cout << "Done solving and writing" << endl;
+    if (rank==0){
+      solution->writeFluxesToFile(ConfusionBilinearForm::U_HAT, "multi_irregular.dat");
+    }
+    
+    mesh->enforceOneIrregularity();
+   
+    // solve
+    solution->solve(false);         
+
+    if (rank==0){
+      solution->writeFluxesToFile(ConfusionBilinearForm::U_HAT, "irregular.dat");
+    }
+
+    cout << "mesh test suite consistency parity test returns (after making mesh regular) " << MeshTestSuite::checkMeshConsistency(*mesh) << endl;
+
+    
+    return 0;
+    
+    // ---------------------- END TEST PARITY ----------------------------
+  }else{
+    solution->solve(false);
+    cout << "Processor " << rank << " returned from solve()." << endl;
   }
 
-
-  int numRefinements = 1;
+  bool limitIrregularity = true;
+  int numRefinements = 3;
   double thresholdFactor = 0.20;
-  
+  int refIterCount = 0;  
   double totalEnergyErrorSquared;
   for (int i=0; i<numRefinements; i++) {
-    vector<int> cellsToRefine;
-    FieldContainer<double> energyError;
+    map<int, double> energyError;
     solution->energyError(energyError);
-/*
-    int numActiveCells = energyError.dimension(0);
-    double maxError = 0.0;
-    totalEnergyErrorSquared = 0.0;
-    for (int activeCellIndex=0; activeCellIndex<numActiveCells; activeCellIndex++) {
-      cout << "energy error for cellID " << mesh->activeElements()[activeCellIndex]->cellID() << " is " << energyError(activeCellIndex) << endl;
-      maxError = max(energyError(activeCellIndex),maxError);
-      totalEnergyErrorSquared += energyError(activeCellIndex) * energyError(activeCellIndex);
-    }
+    vector< Teuchos::RCP< Element > > activeElements = mesh->activeElements();
+    vector< Teuchos::RCP< Element > >::iterator activeElemIt;
 
-    for (int activeCellIndex=0; activeCellIndex<numActiveCells; activeCellIndex++) {
-      if (energyError(activeCellIndex) >= thresholdFactor * maxError ) {
-        int cellID = mesh->activeElements()[activeCellIndex]->cellID();
-        cellsToRefine.push_back(cellID);
-        cout << "refining cell ID " << cellID << " on proc " << rank << endl;
+    // greedy refinement algorithm
+    vector<int> triangleCellsToRefine;
+    vector<int> quadCellsToRefine;
+    double maxError = 0.0;
+    for (activeElemIt = activeElements.begin();activeElemIt != activeElements.end(); activeElemIt++){
+      Teuchos::RCP< Element > current_element = *(activeElemIt);
+      int cellID = current_element->cellID();
+      //      cout << "energy error for cellID " << cellID << " = " << energyError[cellID] << endl;      
+      maxError = max(energyError[cellID],maxError);
+      totalEnergyErrorSquared += energyError[cellID]*energyError[cellID];
+    }
+    cout << "For refinement number " << refIterCount << ", energy error = " << totalEnergyErrorSquared<<endl;
+
+    //actually do refinements
+    for (activeElemIt = activeElements.begin();activeElemIt != activeElements.end(); activeElemIt++){
+      Teuchos::RCP< Element > current_element = *(activeElemIt);
+      int cellID = current_element->cellID();
+      if (energyError[cellID]>=thresholdFactor*maxError){
+	if (current_element->numSides()==3){
+	  triangleCellsToRefine.push_back(cellID);
+	}else if (current_element->numSides()==4){
+	  quadCellsToRefine.push_back(cellID);
+	}
+	if (rank==0){
+	  cout << "refining cell ID " << cellID << endl;
+	}
+      }
+    }    
+    mesh->hRefine(triangleCellsToRefine,RefinementPattern::regularRefinementPatternTriangle());
+    triangleCellsToRefine.clear();
+    mesh->hRefine(quadCellsToRefine,RefinementPattern::regularRefinementPatternQuad());
+    quadCellsToRefine.clear();
+
+    // enforce 1-irregularity if desired
+    if (limitIrregularity){
+      if (refIterCount==2){
+	vector<int> manualCells;
+	manualCells.push_back(12);
+	mesh->hRefine(manualCells,RefinementPattern::regularRefinementPatternQuad());
+      }else{    
+	//	mesh->enforceOneIrregularity();
       }
     }
-
-    if (useTriangles) {
-      mesh->hRefine(cellsToRefine,RefinementPattern::regularRefinementPatternTriangle());
-    } else {
-      mesh->hRefine(cellsToRefine,RefinementPattern::regularRefinementPatternQuad());
-    }
-    cout << "Solving..." << endl;    
+    
+    refIterCount++;
+    cout << "Solving on refinement iteration number " << refIterCount << "..." << endl;    
     solution->solve(false);
     cout << "Solved..." << endl;    
- */
-
-  }
+ } 
   
   if (useEggerSchoeberl) {
     // print out the L2 error of the solution:
@@ -145,7 +199,14 @@ int main(int argc, char *argv[]) {
   }
 
   // save a data file for plotting in MATLAB
-//  solution->writeToFile(ConfusionBilinearForm::U, "Confusion_u_adaptive.dat");
-//  solution->writeFluxesToFile(ConfusionBilinearForm::U_HAT, "Confusion_u_hat_adaptive.dat");
-  
+  if (rank==0){
+    solution->writeToFile(ConfusionBilinearForm::U, "Confusion_u_adaptive.dat");
+    solution->writeFluxesToFile(ConfusionBilinearForm::U_HAT, "Confusion_u_hat_adaptive.dat");
+    cout << "Done writing soln to file." << endl;
+  }
+  if (rank==0){
+    cout << "mesh test suite consistency parity test returns (after making mesh regular) " << MeshTestSuite::checkMeshConsistency(*mesh) << endl;
+  }
+
+  return 0;
 }
