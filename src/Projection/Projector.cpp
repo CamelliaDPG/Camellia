b90a2453f60abdc2416f48616236a7aeee0bd0f7
jchan985 <jchan985@36274a68-8941-4c93-80ef-affaed5139da>
2012-02-01 18:45:53 +0000

    committing changes to projection (it works, at least for the test in MeshTestSuite.cpp)
    git-svn-id: https://svn.ices.utexas.edu/repos/camellia@194 36274a68-8941-4c93-80ef-affaed5139da

--- src/Projection/Projector.cpp
+++ src/Projection/Projector.cpp
@@ -39,23 +39,24 @@ void Projector::projectFunctionOntoBasis(FieldContainer<double> &basisCoefficien
   BasisValueCache basisCache(physicalCellNodes, cellTopo, *(dofOrderPtr), maxTrialDegree, false);
   // assume only L2 projections
   EOperatorExtended op = IntrepidExtendedTypes::OPERATOR_VALUE;
-  
+
   // have information, build inner product matrix
   int numDofs = basis->getCardinality();
   FieldContainer<double> cubPoints = basisCache.getPhysicalCubaturePoints();    
+
   FieldContainer<double> basisValues = *(basisCache.getTransformedValues(basis, op));
-  FieldContainer<double> testBasisValues = *basisCache.getTransformedWeightedValues(basis, op);
-  
-  FieldContainer<double> functionValues;
-  fxn->getValues(functionValues, cubPoints);
-  
+  FieldContainer<double> testBasisValues = *(basisCache.getTransformedWeightedValues(basis, op));
+
   int numCells = physicalCellNodes.dimension(0);
+  int numPts = cubPoints.dimension(1);
+  FieldContainer<double> functionValues;
+  fxn->getValues(functionValues, cubPoints);  
 
   FieldContainer<double> gramMatrix(numCells,numDofs,numDofs);
   FieldContainer<double> ipVector(numCells,numDofs);
   FunctionSpaceTools::integrate<double>(gramMatrix,basisValues,testBasisValues,COMP_CPP);
   FunctionSpaceTools::integrate<double>(ipVector,functionValues,testBasisValues,COMP_CPP); 
-
+  
   basisCoefficients.resize(numCells,numDofs);
   for (int cellIndex=0; cellIndex<numCells; cellIndex++){
 
@@ -68,20 +69,52 @@ void Projector::projectFunctionOntoBasis(FieldContainer<double> &basisCoefficien
 			       gramMatrix.dimension(1)); // stride -- fc stores in row-major order (a.o.t. SDM)
     
     Epetra_SerialDenseVector b(Copy,
-			       &functionValues(cellIndex,0),
-			       functionValues.dimension(1));
+			       &ipVector(cellIndex,0),
+			       ipVector.dimension(1));
     
     Epetra_SerialDenseVector x(functionValues.dimension(1));
     
+    /*
+    cout << "matrix A = " << endl;
+    for (int i=0;i<gramMatrix.dimension(2);i++){
+      for (int j=0;j<gramMatrix.dimension(1);j++){
+	cout << A(i,j) << " ";
+      }
+      cout << endl;
+    }
+    cout << endl;
+
+    cout << "vector B = " << endl;
+    for (int i=0;i<functionValues.dimension(1);i++){
+      cout << b(i) << endl;
+    }
+    */
+
     solver.SetMatrix(A);
     int info = solver.SetVectors(x,b);
     if (info!=0){
       cout << "projectFunctionOntoBasis: failed to SetVectors with error " << info << endl;
     }
+
+    bool equilibrated = false;
+    if (solver.ShouldEquilibrate()){
+      solver.EquilibrateMatrix();
+      solver.EquilibrateRHS();      
+      equilibrated = true;
+    }   
+
     info = solver.Solve();
     if (info!=0){
       cout << "projectFunctionOntoBasis: failed to solve with error " << info << endl;
     }
+
+    if (equilibrated) {
+      int successLocal = solver.UnequilibrateLHS();
+      if (successLocal != 0) {
+        cout << "projection: unequilibration FAILED with error: " << successLocal << endl;
+      }
+    }
+
     for (int i=0;i<numDofs;i++){
       basisCoefficients(cellIndex,i) = x(i);
     }   
