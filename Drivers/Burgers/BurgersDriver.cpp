5beecb5cf3981700c8b7fd52146c9be380d8af6b
jchan985 <jchan985@36274a68-8941-4c93-80ef-affaed5139da>
2012-02-14 22:39:33 +0000

    removing all Confection/confusion items from Burgers
    git-svn-id: https://svn.ices.utexas.edu/repos/camellia@263 36274a68-8941-4c93-80ef-affaed5139da

--- Drivers/Burgers/BurgersDriver.cpp
+++ Drivers/Burgers/BurgersDriver.cpp
@@ -95,7 +95,7 @@ int main(int argc, char *argv[]) {
   Teuchos::RCP<LocalStiffnessMatrixFilter> penaltyBC = Teuchos::rcp(new PenaltyMethodFilter(problem));
   solution->setFilter(penaltyBC);
 
-  int numRefs = 3;
+  int numRefs = 2;
   int refIter = 0;
   for (int refIndex=0;refIndex<numRefs;refIndex++){    
 
@@ -109,11 +109,14 @@ int main(int argc, char *argv[]) {
     bool converged = false;
     while (!converged){ // while energy error has not stabilized
 
+      cout << "solving..." << endl;
       solution->solve();
 
+      cout << "summing background flow..." << endl;
       backgroundFlow->addSolution(solution,1.0);
 
       // see if energy error has stabilized
+      cout << "getting energy error" << endl;
       solution->energyError(energyError);    
       double totalError = 0.0;
       for (activeElemIt = activeElements.begin();activeElemIt != activeElements.end(); activeElemIt++){
@@ -132,6 +135,13 @@ int main(int argc, char *argv[]) {
 	prevError=totalError; // reset previous error and continue
       } 
       i++;
+      
+      /*
+	if (i>1){
+	converged=true;
+      }
+      */
+      
     }
 
     // greedy refinement algorithm - mark cells for refinement
@@ -152,10 +162,45 @@ int main(int argc, char *argv[]) {
 	if (current_element->numSides()==3){
 	  triangleCellsToRefine.push_back(cellID);
 	}else if (current_element->numSides()==4){
+	  if (rank==0){
+	    cout << "refining CellID = " << cellID << endl;
+	  }
 	  quadCellsToRefine.push_back(cellID);
 	}
       }
-    }    
+    }  
+    /*
+    quadCellsToRefine.clear();
+    // shoudl induce hanging on 4x4 mesh initial, np 2
+    if (refIter==0){
+      quadCellsToRefine.push_back(0);
+      quadCellsToRefine.push_back(1);
+      quadCellsToRefine.push_back(6);
+      quadCellsToRefine.push_back(7);
+      quadCellsToRefine.push_back(9);
+      quadCellsToRefine.push_back(10);
+      quadCellsToRefine.push_back(11);
+      quadCellsToRefine.push_back(13);
+      quadCellsToRefine.push_back(14);
+    } else if (refIter==1) {
+      quadCellsToRefine.push_back(16);
+      quadCellsToRefine.push_back(17);
+      quadCellsToRefine.push_back(19);
+      quadCellsToRefine.push_back(24);
+      quadCellsToRefine.push_back(26);
+      quadCellsToRefine.push_back(29);
+      quadCellsToRefine.push_back(30);
+      quadCellsToRefine.push_back(35);
+      quadCellsToRefine.push_back(36);
+      quadCellsToRefine.push_back(39);
+      quadCellsToRefine.push_back(40);
+      quadCellsToRefine.push_back(43);
+      quadCellsToRefine.push_back(45);
+      quadCellsToRefine.push_back(46);
+      quadCellsToRefine.push_back(49);
+      quadCellsToRefine.push_back(50);
+    }
+    */
 
     if (rank==0){
       cout << "refining on iter " << refIter << endl;
@@ -172,16 +217,14 @@ int main(int argc, char *argv[]) {
     triangleCellsToRefine.clear();
     mesh->hRefine(quadCellsToRefine,RefinementPattern::regularRefinementPatternQuad(),solutions);
     quadCellsToRefine.clear();
-    cout << "enforcing one irregularity on rank " << rank << endl;
 
-    mesh->enforceOneIrregularity(solutions);
-    solutions.clear();
+    //    cout << "enforcing one irregularity on rank " << rank << endl;
+    //    mesh->enforceOneIrregularity(solutions);
 
     cout << "discarding old cell coeffs on rank " << rank << endl;
     backgroundFlow->discardInactiveCellCoefficients();
-    if (rank==0){
-      cout << "proceeding to next refinement iteration" << endl;
-    }
+
+    cout << "proceeding to next refinement iteration on rank " << rank << endl;
   }
 
   // one more nonlinear solve on refined mesh
