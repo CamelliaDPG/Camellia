b861c176062c12e1667312b3b6cdeacfa13a6906
Truman Ellis <ellis.truman@gmail.com>
2012-06-13 15:54:11 -0500

    Fixed CMake build system.

---
+++ ConfigureOptions.cmake
@@ -0,0 +1,9 @@
+# Configure file for Camellia
+
+set(TRILINOS_PATH /workspace/jchan/trilinos_builds/mpi_release/)
+set(MUMPS_PATH /workspace/jchan/MUMPS_4.9.2/)
+
+set(CMAKE_CXX_COMPILER mpicxx)
+set(CMAKE_BUILD_TYPE DEBUG)
+set(BUILD_SHARED_LIBS ON)
+set(CMAKE_VERBOSE_MAKEFILE ON)
