353c0b58f0d0d2ba82b38aff6ebe8a62db36a37c
Jesse Chan <jchan985@gmail.com>
2012-07-27 12:36:54 -0500

    adding changes to Confusion_Jesse

--- drivers/Confusion_Jesse/Vortex.cpp
+++ drivers/Confusion_Jesse/Vortex.cpp
@@ -29,23 +29,43 @@ public:
   }
 };
 
-class InflowSquareBoundary : public SpatialFilter {
+class invHScaling : public hFunction {
 public:
-  bool matchesPoint(double x, double y) {
-    double tol = 1e-14;
-    bool xMatch = (abs(x)<tol);
-    bool yMatch = (abs(y)<tol);
-    return xMatch || yMatch;
+  double value(double x, double y, double h) {
+    return 1.0/h;
   }
 };
 
-class OutflowSquareBoundary : public SpatialFilter {
+class InflowSquareBoundary : public SpatialFilter {
+  FunctionPtr _beta;
 public:
-  bool matchesPoint(double x, double y) {
-    double tol = 1e-14;
-    bool xMatch = (abs(x-1.0)<tol);
-    bool yMatch = (abs(y-1.0)<tol);
-    return xMatch || yMatch;
+  InflowSquareBoundary(FunctionPtr beta){
+    _beta = beta;
+  }
+  bool matchesPoints(FieldContainer<bool> &pointsMatch, BasisCachePtr basisCache) {
+    const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());    
+    const FieldContainer<double> *normals = &(basisCache->getSideNormals());
+    int numCells = (*points).dimension(0);
+    int numPoints = (*points).dimension(1);
+
+    FieldContainer<double> beta_pts(numCells,numPoints,2);
+    _beta->values(beta_pts,basisCache);
+
+    double tol=1e-14;
+    bool somePointMatches = false;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+	double n1 = (*normals)(cellIndex,ptIndex,0);
+	double n2 = (*normals)(cellIndex,ptIndex,1);
+	double beta_n = beta_pts(cellIndex,ptIndex,0)*n1 + beta_pts(cellIndex,ptIndex,1)*n2 ;
+	pointsMatch(cellIndex,ptIndex) = false;
+	if (beta_n < 0){
+	  pointsMatch(cellIndex,ptIndex) = true;
+	  somePointMatches = true;
+	}
+      }
+    }
+    return somePointMatches;
   }
 };
 
@@ -63,11 +83,38 @@ public:
       for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
         double x = (*points)(cellIndex,ptIndex,0);
         double y = (*points)(cellIndex,ptIndex,1);
-	if ((abs(x)<tol) || ((abs(y)<tol) && (x<.2))){
-	  values(cellIndex,ptIndex) = 1.0;
-	} else{
-	  values(cellIndex,ptIndex) = 0.0;  
-	}
+	double r = sqrt(x*x + y*y); 
+	values(cellIndex,ptIndex) = r-1.0;
+      }
+    }
+  }
+};
+
+// inflow values for u
+class l2NormOfVector : public Function {
+  FunctionPtr _beta;
+public:
+  l2NormOfVector(FunctionPtr beta) : Function(0){
+    _beta = beta;
+  }
+
+  void values(FieldContainer<double> &values, BasisCachePtr basisCache) {
+    int numCells = values.dimension(0);
+    int numPoints = values.dimension(1);
+
+    FieldContainer<double> beta_pts(numCells,numPoints,2);
+    _beta->values(beta_pts,basisCache);
+
+    const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());
+    double tol=1e-14;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+        double x = (*points)(cellIndex,ptIndex,0);
+        double y = (*points)(cellIndex,ptIndex,1);
+	double b1 =  beta_pts(cellIndex,ptIndex,0);
+	double b2 =  beta_pts(cellIndex,ptIndex,1);
+	double beta_norm =b1*b1 + b2*b2;
+	values(cellIndex,ptIndex) = sqrt(beta_norm);
       }
     }
   }
@@ -84,12 +131,10 @@ public:
     const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());
     for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
       for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
-        for (int d = 0; d < spaceDim; d++) {
-          double x = (*points)(cellIndex,ptIndex,0);
-          double y = (*points)(cellIndex,ptIndex,1);
-          values(cellIndex,ptIndex,0) = y;
-          values(cellIndex,ptIndex,0) = -x;
-        }
+	double x = (*points)(cellIndex,ptIndex,0);
+	double y = (*points)(cellIndex,ptIndex,1);	
+	values(cellIndex,ptIndex,0) = -y;
+	values(cellIndex,ptIndex,1) = x;
       }
     }
   }
@@ -104,6 +149,15 @@ int main(int argc, char *argv[]) {
   int rank = 0;
   int numProcs = 1;
 #endif
+
+  int numRefs = 0;
+  if ( argc > 1) {
+    numRefs = atoi(argv[1]);
+    if (rank==0){
+      cout << "numRefs = " << numRefs << endl;
+    }
+  }
+
   ////////////////////   DECLARE VARIABLES   ///////////////////////
   // define test variables
   VarFactory varFactory; 
@@ -117,13 +171,9 @@ int main(int argc, char *argv[]) {
   VarPtr sigma1 = varFactory.fieldVar("\\sigma_1");
   VarPtr sigma2 = varFactory.fieldVar("\\sigma_2");
   
-  vector<double> beta_const;
-  double c = sqrt(1.25);
-  beta_const.push_back(1.0/c);
-  beta_const.push_back(.5/c);
-//  FunctionPtr beta = Teuchos::rcp(new Beta());
+  FunctionPtr beta = Teuchos::rcp(new Beta());
   
-  double eps = 1e-3;
+  double eps = 1e-4;
   
   ////////////////////   DEFINE BILINEAR FORM   ///////////////////////
 
@@ -132,28 +182,34 @@ int main(int argc, char *argv[]) {
   confusionBF->addTerm(sigma1 / eps, tau->x());
   confusionBF->addTerm(sigma2 / eps, tau->y());
   confusionBF->addTerm(u, tau->div());
-  confusionBF->addTerm(-uhat, tau->dot_normal());
+  confusionBF->addTerm(uhat, -tau->dot_normal());
   
   // v terms:
   confusionBF->addTerm( sigma1, v->dx() );
   confusionBF->addTerm( sigma2, v->dy() );
-  confusionBF->addTerm( beta_const * u, - v->grad() );
+  confusionBF->addTerm( u, - beta * v->grad() );
   confusionBF->addTerm( beta_n_u_minus_sigma_n, v);
   
   ////////////////////   DEFINE INNER PRODUCT(S)   ///////////////////////
 
   // quasi-optimal norm
-  IPPtr qoptIP = Teuchos::rcp(new IP);
+  IPPtr qoptIP = Teuchos::rcp(new IP);  
   qoptIP->addTerm( v );
   qoptIP->addTerm( tau / eps + v->grad() );
-  qoptIP->addTerm( beta_const * v->grad() - tau->div() );
+  qoptIP->addTerm( beta * v->grad() - tau->div() );
 
   // robust test norm
   IPPtr robIP = Teuchos::rcp(new IP);
   FunctionPtr ip_scaling = Teuchos::rcp( new EpsilonScaling(eps) ); 
-  robIP->addTerm( ip_scaling * v );
+  FunctionPtr invH = Teuchos::rcp( new invHScaling ); 
+  FunctionPtr betaNorm = Teuchos::rcp( new l2NormOfVector(beta));
+  FunctionPtr three = Teuchos::rcp( new ConstantScalarFunction(3.0));
+
+  //  robIP->addTerm( ip_scaling * v);
+  robIP->addTerm( eps*invH*v );
+
   robIP->addTerm( sqrt(eps) * v->grad() );
-  robIP->addTerm( beta_const * v->grad() );
+  robIP->addTerm( beta * v->grad() );
   robIP->addTerm( tau->div() );
   robIP->addTerm( ip_scaling/sqrt(eps) * tau );
   
@@ -165,33 +221,31 @@ int main(int argc, char *argv[]) {
 
   ////////////////////   CREATE BCs   ///////////////////////
   Teuchos::RCP<BCEasy> bc = Teuchos::rcp( new BCEasy );
-  SpatialFilterPtr inflowBoundary = Teuchos::rcp( new InflowSquareBoundary );
-  SpatialFilterPtr outflowBoundary = Teuchos::rcp( new OutflowSquareBoundary ); 
+  SpatialFilterPtr inflowBoundary = Teuchos::rcp( new InflowSquareBoundary(beta) );
 
   FunctionPtr u0 = Teuchos::rcp( new U0 );
   FunctionPtr n = Teuchos::rcp( new UnitNormalFunction );
-  bc->addDirichlet(uhat, outflowBoundary, zero);
-  bc->addDirichlet(beta_n_u_minus_sigma_n, inflowBoundary, beta_const*n*u0);
-  
+  //  bc->addDirichlet(uhat, outflowBoundary, zero);
+  bc->addDirichlet(beta_n_u_minus_sigma_n, inflowBoundary, beta*n*u0);  
   // Teuchos::RCP<PenaltyConstraints> pc = Teuchos::rcp(new PenaltyConstraints);
   // pc->addConstraint(uhat==u0,inflowBoundary);
 
   ////////////////////   BUILD MESH   ///////////////////////
   // define nodes for mesh
-  int H1Order = 2, pToAdd = 2;
+  int H1Order = 3, pToAdd = 2;
   
   FieldContainer<double> quadPoints(4,2);
   
-  quadPoints(0,0) = 0.0; // x1
-  quadPoints(0,1) = 0.0; // y1
+  quadPoints(0,0) = -1.0; // x1
+  quadPoints(0,1) = -1.0; // y1
   quadPoints(1,0) = 1.0;
-  quadPoints(1,1) = 0.0;
+  quadPoints(1,1) = -1.0;
   quadPoints(2,0) = 1.0;
   quadPoints(2,1) = 1.0;
-  quadPoints(3,0) = 0.0;
+  quadPoints(3,0) = -1.0;
   quadPoints(3,1) = 1.0;
   
-  int nCells = 2;
+  int nCells = 4;
   int horizontalCells = nCells, verticalCells = nCells;
   
   // create a pointer to a new mesh:
@@ -210,9 +264,7 @@ int main(int argc, char *argv[]) {
   
   double energyThreshold = 0.2; // for mesh refinements
   RefinementStrategy refinementStrategy( solution, energyThreshold );
-  
-  int numRefs = 7;
-    
+   
   for (int refIndex=0; refIndex<numRefs; refIndex++){    
     solution->solve(false);
     refinementStrategy.refine(rank==0); // print to console on rank 0
@@ -221,8 +273,9 @@ int main(int argc, char *argv[]) {
   solution->solve(false);
   
   if (rank==0){
-    solution->writeFieldsToFile(u->ID(), "u.m");
+    //    solution->writeFieldsToFile(u->ID(), "u.m");
     solution->writeFluxesToFile(uhat->ID(), "uhat.dat");
+    solution->writeToVTK("vortex.vtu",min(H1Order+1,4));
     
     cout << "wrote files: u.m, uhat.dat\n";
   }
