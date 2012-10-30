492d1bbe78acc0bd2fa4e742b81f2b53e83ac9e2
Nate Roberts <nate@nateroberts.com>
2012-10-30 18:08:32 -0500

    Made the two-term integration tests more granular (revealing that the issue is in fact that down to said integration not commuting, as opposed to having to do with the boundary/volume terms being summed together).

--- drivers/DPGTests/LinearTermTests.cpp
+++ drivers/DPGTests/LinearTermTests.cpp
@@ -432,9 +432,9 @@ bool LinearTermTests::testBoundaryPlusVolumeTerms() {
   // part c: two-term integrals
   FieldContainer<double> integrals_expected_two_term( mesh->numElements(), num_dofs, num_dofs);
   FieldContainer<double> integrals_actual_two_term( mesh->numElements(), num_dofs, num_dofs );
-  lt_v->integrate(integrals_expected_two_term, testOrder, ibp, testOrder, basisCache, false, false);
   LinearTermPtr ibp1 = vector_fxn * n * v1;
   LinearTermPtr ibp2 = - vector_fxn * v1->grad();
+  lt_v->integrate(integrals_expected_two_term, testOrder, ibp1 + ibp2, testOrder, basisCache, false, false);
   lt_v->integrate(integrals_actual_two_term, testOrder, ibp1, testOrder, basisCache, false, false); // don't forceBoundary, don't sumInto
   lt_v->integrate(integrals_actual_two_term, testOrder, ibp2, testOrder, basisCache, false, true);  // DO sumInto
   
@@ -445,12 +445,32 @@ bool LinearTermTests::testBoundaryPlusVolumeTerms() {
   }
   
   // now, same thing but with the roles of ibp{1|2} and lt_v reversed:
+  (ibp1 + ibp2)->integrate(integrals_expected_two_term, testOrder, lt_v, testOrder, basisCache, false, false);
   ibp1->integrate(integrals_actual_two_term, testOrder, lt_v, testOrder, basisCache, false, false); // don't forceBoundary, don't sumInto
   ibp2->integrate(integrals_actual_two_term, testOrder, lt_v, testOrder, basisCache, false, true);  // DO sumInto
   
   maxDiff = 0;
   if (! fcsAgree(integrals_actual_two_term, integrals_expected_two_term, tol, maxDiff) ) {
     cout << "two-term integration is not bilinear; maxDiff: " << maxDiff << endl;
+    success = false;
+  }
+  
+  // now, test that two-term integration commutes in the two terms:
+  ibp1->integrate(integrals_expected_two_term, testOrder, lt_v, testOrder, basisCache, false, false);
+  lt_v->integrate(integrals_expected_two_term, testOrder, ibp1, testOrder, basisCache, false, false);
+  
+  maxDiff = 0;
+  if (! fcsAgree(integrals_actual_two_term, integrals_expected_two_term, tol, maxDiff) ) {
+    cout << "two-term integration does not commute for boundary value (ibp1); maxDiff: " << maxDiff << endl;
+    success = false;
+  }
+  
+  ibp2->integrate(integrals_expected_two_term, testOrder, lt_v, testOrder, basisCache, false, false);
+  lt_v->integrate(integrals_expected_two_term, testOrder, ibp2, testOrder, basisCache, false, false);
+  
+  maxDiff = 0;
+  if (! fcsAgree(integrals_actual_two_term, integrals_expected_two_term, tol, maxDiff) ) {
+    cout << "two-term integration does not commute for volume value (ibp2); maxDiff: " << maxDiff << endl;
     success = false;
   }
   
@@ -508,12 +528,32 @@ bool LinearTermTests::testBoundaryPlusVolumeTerms() {
   }
 
   // now, same thing but with the roles of ibp{1|2} and lt_v reversed:
+  (ibp1 + ibp2)->integrate(integrals_expected_two_term, testOrder, lt_v, testOrder, basisCache, false, false);
   ibp1->integrate(integrals_actual_two_term, testOrder, lt_v, testOrder, basisCache, false, false); // don't forceBoundary, don't sumInto
   ibp2->integrate(integrals_actual_two_term, testOrder, lt_v, testOrder, basisCache, false, true);  // DO sumInto
   
   maxDiff = 0;
   if (! fcsAgree(integrals_actual_two_term, integrals_expected_two_term, tol, maxDiff) ) {
     cout << "two-term integration is not bilinear; maxDiff: " << maxDiff << endl;
+    success = false;
+  }
+  
+  // now, test that two-term integration commutes in the two terms:
+  ibp1->integrate(integrals_expected_two_term, testOrder, lt_v, testOrder, basisCache, false, false);
+  lt_v->integrate(integrals_expected_two_term, testOrder, ibp1, testOrder, basisCache, false, false);
+  
+  maxDiff = 0;
+  if (! fcsAgree(integrals_actual_two_term, integrals_expected_two_term, tol, maxDiff) ) {
+    cout << "two-term integration does not commute for boundary value (ibp1); maxDiff: " << maxDiff << endl;
+    success = false;
+  }
+  
+  ibp2->integrate(integrals_expected_two_term, testOrder, lt_v, testOrder, basisCache, false, false);
+  lt_v->integrate(integrals_expected_two_term, testOrder, ibp2, testOrder, basisCache, false, false);
+  
+  maxDiff = 0;
+  if (! fcsAgree(integrals_actual_two_term, integrals_expected_two_term, tol, maxDiff) ) {
+    cout << "two-term integration does not commute for volume value (ibp2); maxDiff: " << maxDiff << endl;
     success = false;
   }
   
