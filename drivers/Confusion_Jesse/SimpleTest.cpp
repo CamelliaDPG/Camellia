2569fa94b1aaf43e19a5f0d76db861038aff4211
Jesse Chan <jchan985@gmail.com>
2012-10-30 18:43:32 -0500

    fixing TestRieszRep() and SimpleTest to make them resemble each other

--- drivers/Confusion_Jesse/SimpleTest.cpp
+++ drivers/Confusion_Jesse/SimpleTest.cpp
@@ -5,6 +5,10 @@
 #include "PreviousSolutionFunction.h"
 #include "CondensationSolver.h"
 #include "ZoltanMeshPartitionPolicy.h"
+
+#include "RieszRep.h"
+#include "BasisFactory.h" // for test
+
 #ifdef HAVE_MPI
 #include <Teuchos_GlobalMPISession.hpp>
 #else
@@ -161,6 +165,118 @@ public:
   }
 };
 
+class DivTestFunction : public Function{
+  void values(FieldContainer<double> &values, BasisCachePtr basisCache){
+    int numCells = values.dimension(0);
+    int numPoints = values.dimension(1);
+
+    const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());
+    double tol=1e-14;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+        double x = (*points)(cellIndex,ptIndex,0);
+        double y = (*points)(cellIndex,ptIndex,1);
+	values(cellIndex,ptIndex) = 1.0-2.0*x;// div F = 1.0-2.0*x;
+      }
+    }
+  }
+};
+
+class TestFunction : public Function{  
+  TestFunction():Function(1){}
+
+  /*
+  FunctionPtr dx(){
+    FunctionPtr one = Teuchos::rcp( new ConstantScalarFunction(1.0));
+    FunctionPtr two_x = 2.0*(FunctionPtr)Teuchos::rcp(new Xn(1));
+    return Teuchos::rcp(new VectorizedFunction(one-two_x,Function::zero()));
+  }
+  FunctionPtr dy(){
+    return Teuchos::rcp(new VectorizedFunction(Function::zero(),Function::zero());
+  }
+  */
+  void values(FieldContainer<double> &values, BasisCachePtr basisCache){
+    int numCells = values.dimension(0);
+    int numPoints = values.dimension(1);
+
+    const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());
+    double tol=1e-14;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+        double x = (*points)(cellIndex,ptIndex,0);
+        double y = (*points)(cellIndex,ptIndex,1);
+	values(cellIndex,ptIndex,0) = x*(1.0-x);// div F = 1.0-2.0*x;
+	values(cellIndex,ptIndex,1) = 0.0;// div F = 1.0-2.0*x;
+      }
+    }
+  }
+};
+
+
+class TestFunction1 : public Function{
+  void values(FieldContainer<double> &values, BasisCachePtr basisCache){
+    int numCells = values.dimension(0);
+    int numPoints = values.dimension(1);
+
+    const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());
+    double tol=1e-14;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+        double x = (*points)(cellIndex,ptIndex,0);
+        double y = (*points)(cellIndex,ptIndex,1);
+	//	values(cellIndex,ptIndex) = .5*x;
+	values(cellIndex,ptIndex) = x*(1.0-x);// div F = 1.0-2.0*x;
+      }
+    }
+  }
+};
+
+class TestFunction2 : public Function{
+  void values(FieldContainer<double> &values, BasisCachePtr basisCache){
+    int numCells = values.dimension(0);
+    int numPoints = values.dimension(1);
+
+    const FieldContainer<double> *points = &(basisCache->getPhysicalCubaturePoints());
+    double tol=1e-14;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+        double x = (*points)(cellIndex,ptIndex,0);
+        double y = (*points)(cellIndex,ptIndex,1);
+	//	values(cellIndex,ptIndex) = .5*y;
+	values(cellIndex,ptIndex) = 0.0;
+      }
+    }
+  }
+};
+
+class TestFunctionBoundary : public Function{
+  bool boundaryValueOnly(){ 
+    return true; 
+  }
+  void values(FieldContainer<double> &values, BasisCachePtr basisCache){
+    int numCells = values.dimension(0);
+    int numPoints = values.dimension(1);
+   
+    const FieldContainer<double> normals = basisCache->getSideNormals();
+    const FieldContainer<double> points =  basisCache->getPhysicalCubaturePoints();
+    double tol = 1e-12;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++) {
+        double x = points(cellIndex,ptIndex,0);
+        double y = points(cellIndex,ptIndex,1);
+	double n1 = normals(cellIndex,ptIndex,0);
+	double n2 = normals(cellIndex,ptIndex,1);
+	
+	//	if (abs(x)<tol || abs(x-1.0)<tol || abs(y)<tol || abs(y-1.0)<tol){
+	values(cellIndex,ptIndex) =  (x*(1.0-x))*n1;
+	//	}else{
+	//	  values(cellIndex,ptIndex) =  0.0;
+	//	}
+      }
+    }
+  }
+};
+
 int main(int argc, char *argv[]) {
 #ifdef HAVE_MPI
   Teuchos::GlobalMPISession mpiSession(&argc, &argv,0);
@@ -172,8 +288,28 @@ int main(int argc, char *argv[]) {
   int numProcs = 1;  
   Epetra_SerialComm Comm;
 #endif
- 
+  
+  int nCells = 2;
+  if ( argc > 1) {
+    nCells = atoi(argv[1]);
+    if (rank==0){
+      cout << "numCells = " << nCells << endl;
+    }
+  }
+  
   double eps = .01;
+  if (argc > 2){
+    eps = atof(argv[2]);
+    if (rank==0)
+      cout << "eps = " << eps << endl;
+  }
+
+  double squareSize = 1.0;
+  if (argc > 3){
+    squareSize = atof(argv[3]);
+    if (rank==0)
+      cout << "eps = " << eps << endl;
+  }
 
   ////////////////////   DECLARE VARIABLES   ///////////////////////
   // define test variables
@@ -211,17 +347,19 @@ int main(int argc, char *argv[]) {
 
    // robust test norm
   IPPtr robIP = Teuchos::rcp(new IP);
-  FunctionPtr ip_scaling = Teuchos::rcp( new EpsilonScaling(eps) ); 
-  //  FunctionPtr ip_scaling = Teuchos::rcp( new ConstantScalarFunction(1.0));
+  //  FunctionPtr ip_scaling = Teuchos::rcp( new EpsilonScaling(eps) ); 
+  FunctionPtr ip_scaling = Teuchos::rcp( new ConstantScalarFunction(1.0));
+  FunctionPtr tau_ip_scaling = Teuchos::rcp( new EpsilonScaling(eps) ); 
 
   robIP->addTerm( ip_scaling * v);
   robIP->addTerm( sqrt(eps) * v->grad() );
   robIP->addTerm( beta * v->grad() );
   robIP->addTerm( tau->div() );
-  robIP->addTerm( ip_scaling/sqrt(eps) * tau );
+  robIP->addTerm( tau_ip_scaling/sqrt(eps) * tau );
   
   ////////////////////   SPECIFY RHS   ///////////////////////
-  FunctionPtr zero = Teuchos::rcp( new ConstantScalarFunction(0.0) );
+
+  FunctionPtr zero = Function::constant(0.0);
   Teuchos::RCP<RHSEasy> rhs = Teuchos::rcp( new RHSEasy );
   FunctionPtr f = zero;
   rhs->addTerm( f * v ); // obviously, with f = 0 adding this term is not necessary!
@@ -243,27 +381,19 @@ int main(int argc, char *argv[]) {
 
   ////////////////////   BUILD MESH   ///////////////////////
   // define nodes for mesh
-  int H1Order = 2; int pToAdd = 2;
+  int H1Order = 1; int pToAdd = 1;
   
   FieldContainer<double> quadPoints(4,2);
   
   quadPoints(0,0) = 0.0; // x1
   quadPoints(0,1) = 0.0; // y1
-  quadPoints(1,0) = 1.0;
+  quadPoints(1,0) = squareSize;
   quadPoints(1,1) = 0.0;
-  quadPoints(2,0) = 1.0;
-  quadPoints(2,1) = 1.0;
+  quadPoints(2,0) = squareSize;
+  quadPoints(2,1) = squareSize;
   quadPoints(3,0) = 0.0;
-  quadPoints(3,1) = 1.0;
-  
-  int nCells = 1;
-  if ( argc > 1) {
-    nCells = atoi(argv[1]);
-    if (rank==0){
-      cout << "numCells = " << nCells << endl;
-    }
-  }
-
+  quadPoints(3,1) = squareSize;
+ 
   int horizontalCells = nCells, verticalCells = nCells;
   
   // create a pointer to a new mesh:
@@ -271,21 +401,28 @@ int main(int argc, char *argv[]) {
                                                 confusionBF, H1Order, H1Order+pToAdd);
   mesh->setPartitionPolicy(Teuchos::rcp(new ZoltanMeshPartitionPolicy("HSFC")));
     
+  ElementTypePtr elemType = mesh->getElement(0)->elementType();
+  BasisCachePtr basisCache = Teuchos::rcp(new BasisCache(elemType, mesh));
+  
+  vector<int> cellIDs;
+  cellIDs.push_back(0); 
+  cellIDs.push_back(1);
+  cellIDs.push_back(2);
+  cellIDs.push_back(3);
+  bool createSideCacheToo = true;
+  
+  basisCache->setPhysicalCellNodes(mesh->physicalCellNodes(elemType), cellIDs, createSideCacheToo);
+
+
   ////////////////////   SOLVE & REFINE   ///////////////////////
 
   Teuchos::RCP<Solution> solution;
   solution = Teuchos::rcp( new Solution(mesh, bc, rhs, robIP) );
-
-  Teuchos::RCP<CondensationSolver> condensed = Teuchos::rcp(new CondensationSolver(mesh,solution));
-  Teuchos::RCP<Solver> condensedSolver = Teuchos::rcp(new CondensationSolver(mesh,solution));  
-
-  Epetra_Map timeMap(numProcs,0,Comm);
-  Epetra_Time timer(Comm);
-
+  
   // PREREFINE TWICE
   double energyThreshold = 0.2; // for mesh refinements
   RefinementStrategy refinementStrategy( solution, energyThreshold );
-  int numRefs = 4;
+  int numRefs = 0;
   if (rank==0){
     cout << "refining..." << endl;
   }
@@ -293,29 +430,74 @@ int main(int argc, char *argv[]) {
     solution->solve(false);
     refinementStrategy.refine(rank==0); 
   }
-
-  //  solution->setReportTimingResults(true);
-
-  // =========================================
+  solution->solve(false);  
+  FunctionPtr uCopy = Teuchos::rcp( new PreviousSolutionFunction(solution, u) );
+  FunctionPtr sigma1Copy = Teuchos::rcp( new PreviousSolutionFunction(solution, sigma1) );
+  FunctionPtr sigma2Copy = Teuchos::rcp( new PreviousSolutionFunction(solution, sigma2) );
+  FunctionPtr fnhatCopy = Teuchos::rcp( new PreviousSolutionFunction(solution, beta_n_u_minus_sigma_n));
+  FunctionPtr uhatCopy = Teuchos::rcp( new PreviousSolutionFunction(solution, uhat));
+  FunctionPtr minusOne = Teuchos::rcp(new ConstantScalarFunction(-1.0));
   
-  timer.ResetStartTime();
-  solution->solve(false);
+  ////////////////////   get residual   ///////////////////////
 
-  cout << "time for LU solve on proc " << rank << " is " << timer.ElapsedTime() << endl;
-  cout << "solution energy error for LU = " << solution->energyErrorTotal() << endl;
+  vector<double> e1(2); // (1,0)
+  vector<double> e2(2); // (0,1)
+  e1[0] = 1;
+  e2[1] = 1;
+  LinearTermPtr residual = Teuchos::rcp(new LinearTerm);// residual
+  LinearTermPtr residualIBP = Teuchos::rcp(new LinearTerm);// residual
 
-  // =========================================
+  FunctionPtr X = Teuchos::rcp(new Xn(1));
+  FunctionPtr Y = Teuchos::rcp(new Yn(1));
+  FunctionPtr testFxn1 = X;
+  FunctionPtr testFxn2 = Y;
+  FunctionPtr divTestFxn = testFxn1->dx() + testFxn2->dy();
+  FunctionPtr vectorTest = testFxn1*e1 + testFxn2*e2;
+  residual->addTerm(divTestFxn*v);
+  residualIBP->addTerm(vectorTest*n*v - vectorTest*v->grad() ); // boundary term
+  
+  /*
+  residual->addTerm(minusOne*beta*uCopy*v->grad());
+  residual->addTerm((sigma1Copy*e1 + sigma2Copy*e2)*v->grad());
+  residual->addTerm(fnhatCopy*v);
 
-  // =========================================
+  residual->addTerm(minusOne*uhatCopy*tau->dot_normal());
+  residual->addTerm((sigma1Copy*e1 + sigma2Copy*e2)*tau);
+  residual->addTerm(uCopy*tau->div());
+  */
 
-  timer.ResetStartTime();
-  solution->condensedSolve();
-  condensed->writeFieldFluxIndsToFile();
+  IPPtr sobolevIP = Teuchos::rcp(new IP);
+  sobolevIP->addTerm(v);
+  //  sobolevIP->addTerm(v->grad());
+  sobolevIP->addTerm(tau);
+  sobolevIP->addTerm(tau->div());
+  Teuchos::RCP<RieszRep> riesz = Teuchos::rcp(new RieszRep(mesh, sobolevIP, residual));
+  riesz->computeRieszRep();
+  Teuchos::RCP<RieszRep> rieszIBP = Teuchos::rcp(new RieszRep(mesh, sobolevIP, residualIBP));
+  rieszIBP->computeRieszRep();
 
-  cout << "time for condensed solve on proc " << rank << " is " << timer.ElapsedTime() << endl;
-  cout << "solution energy error for condensed = " << solution->energyErrorTotal() << endl;
+  FunctionPtr rieszRepFxn = Teuchos::rcp(new RepFunction(v->ID(),riesz));
+  FunctionPtr rieszRepIBPFxn = Teuchos::rcp(new RepFunction(v->ID(),rieszIBP));
+
+  int numCells = basisCache->getPhysicalCubaturePoints().dimension(0);
+  int numPts = basisCache->getPhysicalCubaturePoints().dimension(1);
+  FieldContainer<double> valOriginal( numCells, numPts);
+  FieldContainer<double> valIBP( numCells, numPts);
+  rieszRepFxn->values(valOriginal,basisCache);
+  rieszRepIBPFxn->values(valIBP,basisCache);
+
+  double maxDiff = 0.0;
+  double tol = 1e-15;
+  for (int i = 0;i<numCells;i++){
+    for (int j = 0;j<numPts;j++){
+      maxDiff = max(abs(valOriginal(i,j)-valIBP(i,j)),maxDiff);
+    }
+  }
+  cout << "max diff = " << maxDiff << endl;
 
   if (rank==0){
+    rieszRepFxn->writeValuesToMATLABFile(mesh, "rieszRep.m");
+    rieszRepIBPFxn->writeValuesToMATLABFile(mesh, "rieszRepIBP.m");
     solution->writeFluxesToFile(uhat->ID(), "uhatCond.dat");
     solution->writeFluxesToFile(beta_n_u_minus_sigma_n->ID(), "fhatCond.dat");
     solution->writeToVTK("solnCond.vtu",min(H1Order+1,4));
--- drivers/DPGTests/LinearTermTests.cpp
+++ drivers/DPGTests/LinearTermTests.cpp
@@ -136,12 +136,12 @@ void LinearTermTests::teardown() {
 }
 
 void LinearTermTests::runTests(int &numTestsRun, int &numTestsPassed) { 
-  setup();
+  //  setup();
   if (testRieszInversion()) {
     numTestsPassed++;
   }
   numTestsRun++;
-  teardown();
+  //  teardown();
   
   setup();
   if (testBoundaryPlusVolumeTerms()) {
@@ -543,6 +543,73 @@ bool LinearTermTests::testEnergyNorm() {
 bool LinearTermTests::testRieszInversion() {
   bool success = true;
   
+  ////////////////////   DECLARE VARIABLES   ///////////////////////
+  // define test variables
+  VarFactory varFactory; 
+  VarPtr tau = varFactory.testVar("\\tau", HDIV);
+  VarPtr v = varFactory.testVar("v", HGRAD);
+  
+  // define trial variables
+  VarPtr uhat = varFactory.traceVar("\\widehat{u}");
+  VarPtr beta_n_u_minus_sigma_n = varFactory.fluxVar("\\widehat{\\beta \\cdot n u - \\sigma_{n}}");
+  VarPtr u = varFactory.fieldVar("u");
+  VarPtr sigma1 = varFactory.fieldVar("\\sigma_1");
+  VarPtr sigma2 = varFactory.fieldVar("\\sigma_2");
+
+  vector<double> beta;
+  beta.push_back(1.0);
+  beta.push_back(0.0);
+  double eps = .01;
+  
+  ////////////////////   DEFINE BILINEAR FORM   ///////////////////////
+
+  BFPtr confusionBF = Teuchos::rcp( new BF(varFactory) );
+  // tau terms:
+  confusionBF->addTerm(sigma1 / eps, tau->x());
+  confusionBF->addTerm(sigma2 / eps, tau->y());
+  confusionBF->addTerm(u, tau->div());
+  confusionBF->addTerm(uhat, -tau->dot_normal());
+  
+  // v terms:
+  confusionBF->addTerm( sigma1, v->dx() );
+  confusionBF->addTerm( sigma2, v->dy() );
+  confusionBF->addTerm( -u, beta * v->grad() );
+  confusionBF->addTerm( beta_n_u_minus_sigma_n, v);
+
+  ////////////////////   BUILD MESH   ///////////////////////
+  // define nodes for mesh
+  int H1Order = 1; int pToAdd = 1;
+  
+  FieldContainer<double> quadPoints(4,2);
+  
+  quadPoints(0,0) = 0.0; // x1
+  quadPoints(0,1) = 0.0; // y1
+  quadPoints(1,0) = 1.0;
+  quadPoints(1,1) = 0.0;
+  quadPoints(2,0) = 1.0;
+  quadPoints(2,1) = 1.0;
+  quadPoints(3,0) = 0.0;
+  quadPoints(3,1) = 1.0;
+ 
+  int nCells = 2;
+  int horizontalCells = nCells, verticalCells = nCells;
+  // create a pointer to a new mesh:
+  Teuchos::RCP<Mesh> myMesh = Mesh::buildQuadMesh(quadPoints, horizontalCells, verticalCells,
+						  confusionBF, H1Order, H1Order+pToAdd);    
+
+  ElementTypePtr elemType = myMesh->getElement(0)->elementType();
+  BasisCachePtr basisCache = Teuchos::rcp(new BasisCache(elemType, myMesh));
+  
+  vector<int> cellIDs;
+  cellIDs.push_back(0); 
+  cellIDs.push_back(1);
+  cellIDs.push_back(2);
+  cellIDs.push_back(3);
+  bool createSideCacheToo = true;
+  
+  basisCache->setPhysicalCellNodes(myMesh->physicalCellNodes(elemType), cellIDs, createSideCacheToo);
+
+
   LinearTermPtr integrand = Teuchos::rcp(new LinearTerm);// residual
   LinearTermPtr integrandIBP = Teuchos::rcp(new LinearTerm);// residual
   LinearTermPtr integrandIBPReordered = Teuchos::rcp(new LinearTerm);// residual
@@ -559,30 +626,29 @@ bool LinearTermTests::testRieszInversion() {
   FunctionPtr divTestFxn = testFxn1->dx() + testFxn2->dy();
   FunctionPtr vectorTest = testFxn1*e1 + testFxn2*e2;
 
-  integrand->addTerm(divTestFxn*v1);
-  integrandIBP->addTerm(vectorTest*n*v1 + -vectorTest*v1->grad()); // boundary term
-  integrandIBPReordered->addTerm(-vectorTest*v1->grad() + vectorTest*n*v1); // boundary term
+  integrand->addTerm(divTestFxn*v);
+  integrandIBP->addTerm(vectorTest*n*v + -vectorTest*v->grad()); // boundary term
+  integrandIBPReordered->addTerm(-vectorTest*v->grad() + vectorTest*n*v); // boundary term
 
   IPPtr sobolevIP = Teuchos::rcp(new IP);
-  sobolevIP->addTerm(v1);
-  sobolevIP->addTerm(q1); // dummy var - shouldn't show up in solves
+  sobolevIP->addTerm(v);
 
-  Teuchos::RCP<RieszRep> riesz = Teuchos::rcp(new RieszRep(mesh, sobolevIP, integrand));
+  Teuchos::RCP<RieszRep> riesz = Teuchos::rcp(new RieszRep(myMesh, sobolevIP, integrand));
   riesz->computeRieszRep();
-  Teuchos::RCP<RieszRep> rieszIBP = Teuchos::rcp(new RieszRep(mesh, sobolevIP, integrandIBP));
+  Teuchos::RCP<RieszRep> rieszIBP = Teuchos::rcp(new RieszRep(myMesh, sobolevIP, integrandIBP));
   rieszIBP->computeRieszRep();
-  Teuchos::RCP<RieszRep> rieszIBPReorder = Teuchos::rcp(new RieszRep(mesh, sobolevIP, integrandIBPReordered));
+  Teuchos::RCP<RieszRep> rieszIBPReorder = Teuchos::rcp(new RieszRep(myMesh, sobolevIP, integrandIBPReordered));
   rieszIBPReorder->computeRieszRep();
 
-  FunctionPtr rieszOrigFxn = Teuchos::rcp(new RepFunction(v1->ID(),riesz));
-  FunctionPtr rieszIBPFxn = Teuchos::rcp(new RepFunction(v1->ID(),rieszIBP));
-  FunctionPtr rieszIBPReorderFxn = Teuchos::rcp(new RepFunction(v1->ID(),rieszIBPReorder));
-  int nCells = basisCache->getPhysicalCubaturePoints().dimension(0);
+  FunctionPtr rieszOrigFxn = Teuchos::rcp(new RepFunction(v->ID(),riesz));
+  FunctionPtr rieszIBPFxn = Teuchos::rcp(new RepFunction(v->ID(),rieszIBP));
+  FunctionPtr rieszIBPReorderFxn = Teuchos::rcp(new RepFunction(v->ID(),rieszIBPReorder));
+  int numCells = basisCache->getPhysicalCubaturePoints().dimension(0);
   int numPts = basisCache->getPhysicalCubaturePoints().dimension(1);
 
-  FieldContainer<double> valOriginal( nCells, numPts);
-  FieldContainer<double> valIBP( nCells, numPts);
-  FieldContainer<double> valIBPReorder( nCells, numPts);
+  FieldContainer<double> valOriginal( numCells, numPts);
+  FieldContainer<double> valIBP( numCells, numPts);
+  FieldContainer<double> valIBPReorder( numCells, numPts);
   rieszOrigFxn->values(valOriginal,basisCache);
   rieszIBPFxn->values(valIBP,basisCache);
   rieszIBPReorderFxn->values(valIBPReorder,basisCache);
@@ -603,6 +669,7 @@ bool LinearTermTests::testRieszInversion() {
   }
   return success;
 }
+
 std::string LinearTermTests::testSuiteName() {
   return "LinearTermTests";
 }
