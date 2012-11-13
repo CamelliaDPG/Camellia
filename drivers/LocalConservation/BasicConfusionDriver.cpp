23e325f86bc66dfe229c4cdfe2f23b1f4acf4eef
Truman Ellis <ellis.truman@gmail.com>
2012-11-13 11:15:25 -0600

    Scrapping ProblemTemplates approach. Too buggy.

--- drivers/LocalConservation/BasicConfusionDriver.cpp
+++
@@ -1,9 +0,0 @@
-#include "BasicConfusionProblem.h"
-
-int main(int argc, char *argv[]) {
-  for (int i=0; i < argc; i++)
-    cout << argv[i] << endl;
-  BasicConfusionProblem confusionProb;
-  confusionProb.init();
-  confusionProb.runProblem(argc, argv);
-};
