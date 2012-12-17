5b0354cca85fb7e2897006c9425a54c46160056f
Jesse Chan <jchan985@gmail.com>
2012-12-17 10:59:56 -0600

    removed a redundancy in ScratchPadTests header

--- drivers/DPGTests/ScratchPadTests.h
+++ drivers/DPGTests/ScratchPadTests.h
@@ -51,7 +51,6 @@ public:
   bool testLinearTermEvaluationConsistency();
   bool testIntegrateDiscontinuousFunction();
   bool testGalerkinOrthogonality();
-  bool testErrorRepConsistency();
 
   std::string testSuiteName();
 };
