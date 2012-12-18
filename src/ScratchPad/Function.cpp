03137d591d48044ac26dde0bb01e4880ac9190f3
Nate Roberts <nate@nateroberts.com>
2012-12-18 14:59:48 -0600

    Switch PolarizedFunction to operate on the range [0, 2 pi] instead of [-pi,pi].

--- src/ScratchPad/Function.cpp
+++ src/ScratchPad/Function.cpp
@@ -1025,6 +1025,8 @@ bool PolarizedFunction::isZero() {
 
 void PolarizedFunction::values(FieldContainer<double> &values, BasisCachePtr basisCache) {
   CHECK_VALUES_RANK(values);
+  static const double PI  = 3.141592653589793238462;
+  
   int numCells = values.dimension(0);
   int numPoints = values.dimension(1);
   
@@ -1037,8 +1039,8 @@ void PolarizedFunction::values(FieldContainer<double> &values, BasisCachePtr bas
       double r = sqrt(x * x + y * y);
       double theta = acos(x/r);
       // now x = r cos theta, but need to guarantee that y = r sin theta (might differ in sign)
-      // according to the acos docs, theta will be in [0, pi], so the rule is: (y < 0) --> theta *= -1;
-      if (y < 0) theta *= -1.0;
+      // according to the acos docs, theta will be in [0, pi], so the rule is: (y < 0) --> theta := 2 pi - theta;
+      if (y < 0) theta = 2*PI-theta;
       
       polarPoints(cellIndex, ptIndex, 0) = r;
       polarPoints(cellIndex, ptIndex, 1) = theta;
