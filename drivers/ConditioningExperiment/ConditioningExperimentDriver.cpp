953c2533d10724ce0864745696152d1e9242fb62
Nate Roberts <nate@nateroberts.com>
2013-03-19 17:19:59 -0500

    Renamed ConditioningExperimentDriver, etc.

---
+++ drivers/ConditioningExperiment/ConditioningExperimentDriver.cpp
@@ -0,0 +1,9 @@
+//
+//  StokesConditioningDriver.cpp
+//  Camellia-debug
+//
+//  Created by Nathan Roberts on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
