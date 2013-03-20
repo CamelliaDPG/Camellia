d03f04a17de9761ea5bbb72e65139ba704202009
Nate Roberts <nate@nateroberts.com>
2013-03-20 11:20:09 -0500

    Renamed StokesConditioning directory...

--- drivers/StokesConditioning/StokesConditioningDriver.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  StokesConditioningDriver.cpp
-//  Camellia-debug
-//
-//  Created by Nathan Roberts on 3/12/13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
