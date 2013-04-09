f9aa4bcfa0a10689266fae9356303c94b21849db
Nate Roberts <nate@nateroberts.com>
2013-04-09 12:21:20 -0500

    Tweaked copyright claim.

--- src/Basis/BasisFactory.cpp
+++ src/Basis/BasisFactory.cpp
@@ -1,6 +1,6 @@
 // @HEADER
 //
-// Copyright © 2011 Sandia Corporation. All Rights Reserved.
+// Original version copyright © 2011 Sandia Corporation. All Rights Reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are 
 // permitted provided that the following conditions are met:
