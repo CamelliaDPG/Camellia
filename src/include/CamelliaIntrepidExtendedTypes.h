1acb5e6dacffb16bd72fa622186fc20ec68ba844
Nate Roberts <nate@nateroberts.com>
2013-04-16 09:20:03 -0500

    DIV_FREE space enum definition.

--- src/include/CamelliaIntrepidExtendedTypes.h
+++ src/include/CamelliaIntrepidExtendedTypes.h
@@ -32,7 +32,7 @@ namespace IntrepidExtendedTypes {
     OP_VECTORIZE_VALUE  // 26
   };
   
-  enum EFunctionSpaceExtended { // all but the last two copied from EFunctionSpace
+  enum EFunctionSpaceExtended { // all but the last three copied from EFunctionSpace
     FUNCTION_SPACE_HGRAD = 0,
     FUNCTION_SPACE_HCURL,
     FUNCTION_SPACE_HDIV,
@@ -42,6 +42,7 @@ namespace IntrepidExtendedTypes {
     FUNCTION_SPACE_VECTOR_HVOL,
     FUNCTION_SPACE_TENSOR_HVOL,
     FUNCTION_SPACE_ONE,
+    FUNCTION_SPACE_HDIV_FREE,
     FUNCTION_SPACE_UNKNOWN
   };
   
