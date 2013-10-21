f1cf6d157952d6b0541aedb87e83c2fe67b934e3
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2013-10-21 16:36:41 -0500

    Further work toward a single-element Laplace computation in 3D.

--- drivers/DemoDrivers/Laplace3DSingleElement.cpp
+++ drivers/DemoDrivers/Laplace3DSingleElement.cpp
@@ -28,10 +28,9 @@ int main(int argc, char *argv[]) {
   Teuchos::GlobalMPISession mpiSession(&argc, &argv,0);
   int rank = mpiSession.getRank();
   
-  int minPolyOrder = 1;
+  int minPolyOrder = 2;
   int maxPolyOrder = 5;
   int pToAdd = 2;
-  double sideLength = 10;
 
   if (rank==0) {
     cout << "minPolyOrder: " << minPolyOrder << "\n";
@@ -96,29 +95,178 @@ int main(int argc, char *argv[]) {
     bf->printTrialTestInteractions();
   }
   
+  // for now, let's use the reference cell.  (Jacobian should be the identity.)
+  FieldContainer<double> cubePoints(8,3);
+  cubePoints(0,0) = -1;
+  cubePoints(0,1) = -1;
+  cubePoints(0,2) = -1;
+  
+  cubePoints(1,0) = 1;
+  cubePoints(1,1) = -1;
+  cubePoints(1,2) = -1;
+  
+  cubePoints(2,0) = 1;
+  cubePoints(2,1) = 1;
+  cubePoints(2,2) = -1;
+  
+  cubePoints(3,0) = -1;
+  cubePoints(3,1) = 1;
+  cubePoints(3,2) = -1;
+  
+  cubePoints(4,0) = -1;
+  cubePoints(4,1) = -1;
+  cubePoints(4,2) = 1;
+  
+  cubePoints(5,0) = 1;
+  cubePoints(5,1) = -1;
+  cubePoints(5,2) = 1;
+  
+  cubePoints(6,0) = 1;
+  cubePoints(6,1) = 1;
+  cubePoints(6,2) = 1;
+  
+  cubePoints(7,0) = -1;
+  cubePoints(7,1) = 1;
+  cubePoints(7,2) = 1;
+  
+  cubePoints(0,0) = -1;
+  cubePoints(0,1) = -1;
+  cubePoints(0,2) = -1;
+  
+  cubePoints(0,0) = -1;
+  cubePoints(0,1) = -1;
+  cubePoints(0,2) = -1;
+  
+  cubePoints.resize(1,8,3); // first argument is cellIndex; we'll just have 1
+  
+  Teuchos::RCP<shards::CellTopology> hexTopoPtr;
+  hexTopoPtr = Teuchos::rcp(new shards::CellTopology(shards::getCellTopologyData<shards::Hexahedron<8> >() ));
+
+  int sideDim = 2;
+  int delta_k = 2;
+  
   for (int polyOrder=minPolyOrder; polyOrder<=maxPolyOrder; polyOrder++) {
-    FieldContainer<double> cubePoints(8,3);
-    int ptIndex = 0;
-    for (int i=0; i<2; i++) {
-      double xValue = (i==0) ? -sideLength : sideLength;
-      for (int j=0; j<2; j++) {
-        double yValue = (j==0) ? -sideLength : sideLength;
-        for (int k=0; k<2; k++) {
-          double zValue = (k==0) ? -sideLength : sideLength;
-          cubePoints(ptIndex,0) = xValue;
-          cubePoints(ptIndex,1) = yValue;
-          cubePoints(ptIndex,2) = zValue;
-          ptIndex++;
+    BasisPtr hGradBasisQuad = BasisFactory::getBasis(polyOrder, shards::Quadrilateral<4>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HGRAD);
+    BasisPtr l2BasisQuad = BasisFactory::getBasis(polyOrder, shards::Quadrilateral<4>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HVOL);
+    
+    BasisPtr hGradBasisHex = BasisFactory::getBasis(polyOrder, shards::Hexahedron<8>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HGRAD);
+    BasisPtr hDivBasisHex = BasisFactory::getBasis(polyOrder, shards::Hexahedron<8>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HDIV);
+    BasisPtr l2BasisHex = BasisFactory::getBasis(polyOrder, shards::Hexahedron<8>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HVOL);
+
+    BasisPtr hGradBasisHexTest = BasisFactory::getBasis(polyOrder + delta_k, shards::Hexahedron<8>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HGRAD);
+    BasisPtr hDivBasisHexTest = BasisFactory::getBasis(polyOrder + delta_k, shards::Hexahedron<8>::key, IntrepidExtendedTypes::FUNCTION_SPACE_HDIV);
+    
+    // Define trial discretization
+    DofOrderingPtr trialOrderPtr = Teuchos::rcp( new DofOrdering );
+    trialOrderPtr->addEntry(phi->ID(), l2BasisHex, l2BasisQuad->rangeRank());
+    trialOrderPtr->addEntry(psi1->ID(), l2BasisHex, l2BasisQuad->rangeRank());
+    trialOrderPtr->addEntry(psi2->ID(), l2BasisHex, l2BasisQuad->rangeRank());
+    trialOrderPtr->addEntry(psi3->ID(), l2BasisHex, l2BasisQuad->rangeRank());
+    
+    // traces:
+    for (int sideOrdinal = 0; sideOrdinal < hexTopoPtr->getSideCount(); sideOrdinal++) {
+      trialOrderPtr->addEntry(phi_hat->ID(), hGradBasisQuad, l2BasisQuad->rangeRank(), sideOrdinal);
+      trialOrderPtr->addEntry(psi_hat_n->ID(), l2BasisQuad, l2BasisQuad->rangeRank(), sideOrdinal);
+    }
+    
+    // for the trace of H^1 (phi_hat), need to add identifications of vertex and edge dofs.
+    map< unsigned, vector< pair<unsigned, unsigned> > > vertexNodeToSideVertex;  // key: hex's node #.  Values: sideOrdinal, quad's vertex #.
+    map< unsigned, vector< pair<unsigned, unsigned> > > edgeNodeToSideEdge;  // key: hex's node #.  Values: sideOrdinal, quad's edge #.
+
+    int vertexDim = 0;
+    int edgeDim = 1;
+    
+    for (int sideOrdinal = 0; sideOrdinal < hexTopoPtr->getSideCount(); sideOrdinal++) {
+      shards::CellTopology sideTopo(hexTopoPtr->getCellTopologyData(sideDim, sideOrdinal));
+      for (int edgeOrdinal=0; edgeOrdinal < sideTopo.getEdgeCount(); edgeOrdinal++) {
+        // how does the hex identify this edge?
+        unsigned hexNodeForEdge = hexTopoPtr->getNodeMap(edgeDim, sideOrdinal, edgeOrdinal);
+        if (edgeNodeToSideEdge.find(hexNodeForEdge) == edgeNodeToSideEdge.end()) {
+          edgeNodeToSideEdge[hexNodeForEdge] = vector< pair<unsigned, unsigned> >();
+        }
+        edgeNodeToSideEdge[hexNodeForEdge].push_back(make_pair(sideOrdinal, edgeOrdinal));
+      }
+      for (int vertexOrdinal=0; vertexOrdinal < sideTopo.getVertexCount(); vertexOrdinal++) {
+        // how does the hex identify this vertex?
+        unsigned hexNodeForVertex = hexTopoPtr->getNodeMap(vertexDim, sideOrdinal, vertexOrdinal);
+        if (vertexNodeToSideVertex.find(hexNodeForVertex) == vertexNodeToSideVertex.end()) {
+          vertexNodeToSideVertex[hexNodeForVertex] = vector< pair<unsigned, unsigned> >();
+        }
+        vertexNodeToSideVertex[hexNodeForVertex].push_back(make_pair(sideOrdinal, vertexOrdinal));
+      }
+    }
+    // now, we want to identify all those dofs:
+    for (map< unsigned, vector< pair<unsigned, unsigned> > >::iterator vertexNodeIter = vertexNodeToSideVertex.begin();
+         vertexNodeIter != vertexNodeToSideVertex.end(); vertexNodeIter++) {
+      vector< pair<unsigned, unsigned> > vertices = vertexNodeIter->second;
+      vector< pair<unsigned, unsigned> >::iterator verticesIter = vertices.begin();
+      unsigned firstSideOrdinal = verticesIter->first;
+      unsigned firstVertexOrdinal = verticesIter->second;
+      unsigned firstVertexDofOrdinal = trialOrderPtr->getBasis(phi_hat->ID(), firstSideOrdinal)
+                                                    ->getDofOrdinal(vertexDim, firstVertexOrdinal, 0); // 0: only one vertex dof, so its index is 0
+      
+      while (++verticesIter != vertices.end()) {
+        unsigned sideOrdinal = verticesIter->first;
+        unsigned vertexOrdinal = verticesIter->second;
+        unsigned vertexDofOrdinal = trialOrderPtr->getBasis(phi_hat->ID(), sideOrdinal)
+                                                 ->getDofOrdinal(vertexDim, vertexOrdinal, 0); // 0: only one vertex dof, so its index is 0
+        trialOrderPtr->addIdentification(phi_hat->ID(), firstSideOrdinal, firstVertexDofOrdinal,
+                                         sideOrdinal, vertexDofOrdinal);
+      }
+    }
+
+    for (map< unsigned, vector< pair<unsigned, unsigned> > >::iterator edgeNodeIter = edgeNodeToSideEdge.begin();
+         edgeNodeIter != edgeNodeToSideEdge.end(); edgeNodeIter++) {
+      // there could be a bunch of these (or 0, for linear elements).  How do we determine the correct permutation?
+      // for now, we make the assumption that the layout is CCW or CW consistently for all sides of the element, which
+      // would mean that the orderings are reversed
+      vector< pair<unsigned, unsigned> > edges = edgeNodeIter->second;
+      vector< pair<unsigned, unsigned> >::iterator edgeIter = edges.begin();
+      unsigned firstSideOrdinal = edgeIter->first;
+      unsigned firstEdgeOrdinal = edgeIter->second;
+      BasisPtr firstBasis = trialOrderPtr->getBasis(phi_hat->ID(), firstSideOrdinal);
+      
+      int numDofs = trialOrderPtr->getBasis(phi_hat->ID(), firstSideOrdinal)->dofOrdinalsForSubcell(edgeDim, firstEdgeOrdinal).size();
+
+      while (++edgeIter != edges.end()) {
+        unsigned sideOrdinal = edgeIter->first;
+        unsigned edgeOrdinal = edgeIter->second;
+        for (int dofNumber=0; dofNumber<numDofs; dofNumber++) {
+          // this is the point where we make an assumption about the relative permutation of the identified bases...
+          // when we do this in a mesh, we'll need to do something more sophisticated.
+          unsigned firstDofOrdinal = trialOrderPtr->getBasis(phi_hat->ID(), firstSideOrdinal)
+                                               ->getDofOrdinal(edgeDim, edgeOrdinal, numDofs - dofNumber - 1);
+          unsigned edgeDofOrdinal = trialOrderPtr->getBasis(phi_hat->ID(), sideOrdinal)
+                                                 ->getDofOrdinal(edgeDim, edgeOrdinal, dofNumber);
+          trialOrderPtr->addIdentification(phi_hat->ID(), firstSideOrdinal, firstDofOrdinal,
+                                           sideOrdinal, edgeDofOrdinal);
+          
         }
       }
     }
+    
+    // Define test discretization
+    DofOrderingPtr testOrderPtr = Teuchos::rcp( new DofOrdering );
+    testOrderPtr->addEntry(q->ID(), hDivBasisHexTest, hDivBasisHex->rangeRank());
+    testOrderPtr->addEntry(v->ID(), hGradBasisHexTest, hGradBasisHex->rangeRank());
+    
+    cout << "For k = " << polyOrder - 1 << ", " << trialOrderPtr->totalDofs() << " trial dofs and " << testOrderPtr->totalDofs() << " test dofs.\n";
+    
+    // Create ElementType for cube
+    Teuchos::RCP<ElementType> elemTypePtr = Teuchos::rcp( new ElementType(trialOrderPtr, testOrderPtr, hexTopoPtr) );
+    
+    // Create BasisCache for ElementType and cubePoints
+    BasisCachePtr basisCache = Teuchos::rcp( new BasisCache(elemTypePtr) );
+    basisCache->setPhysicalCellNodes(cubePoints, vector<int>(1,1), true);
+    
+    
   }
-  
+
   // Steps to solve:
   // 1. Define trial discretization
   // 2. Define test discretization
   // 3. Create ElementType for cube
-  // 4. Create BasisCache for ElementType
+  // 4. Create BasisCache for ElementType and cubePoints
   // 5. Mimic/copy Solution's treatment of stiffness and optimal test functions.
   // 6. Apply BCs
   // 7. Invert stiffness (using SerialDenseMatrixUtility, say)
