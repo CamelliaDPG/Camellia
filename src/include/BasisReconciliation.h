1955dd6fa04de2c31d9c6149b51b35c99b482571
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-01-14 16:35:07 -0600

    Changes to BasisReconciliation from yesterday (omitted in earlier commit).

--- src/include/BasisReconciliation.h
+++ src/include/BasisReconciliation.h
@@ -29,37 +29,43 @@ class BasisReconciliation {
   
   typedef pair< Camellia::Basis<>*, int > SideBasisRestriction;
   // cached values:
-  map< pair< Camellia::Basis<>*, Camellia::Basis<>*>, FieldContainer<double> > _simpleReconciliationWeights; // simple: no sides involved
+  typedef pair< Camellia::Basis<>*, Camellia::Basis<>*> BasisPair; // fineBasis first.
+  map< BasisPair, FieldContainer<double> > _simpleReconciliationWeights; // simple: no sides involved
   map< pair< pair< SideBasisRestriction, SideBasisRestriction >, unsigned >, SubBasisReconciliationWeights > _sideReconciliationWeights;
+  
+  // for the caching interface, may want to use vector< pair< RefinementPattern*, int childIndex > > to define the coarse element's neighbor refinement Branch.  Probably still need a vertexPermutation
+  // as well to fully define the relationship.  Something like:
+private:
+  typedef pair< BasisPair, RefinementBranch > RefinedBasisPair; // fineBasis (the one on the refined element) is first in the BasisPair
+  typedef pair< pair< SideBasisRestriction, SideBasisRestriction >, RefinementBranch > SideRefinedBasisPair;
+  map< RefinedBasisPair, FieldContainer<double> > _simpleReconcilationWeights_h;
+  map< pair< SideRefinedBasisPair, unsigned> , SubBasisReconciliationWeights > _sideReconcilationWeights_h;  // unsigned: the vertexPermutation
 public:
   BasisReconciliation(bool cacheResults = true) { _cacheResults = cacheResults; }
 
+  // p
   const FieldContainer<double> &constrainedWeights(BasisPtr finerBasis, BasisPtr coarserBasis); // requires these to be defined on the same topology
   const SubBasisReconciliationWeights &constrainedWeights(BasisPtr finerBasis, int finerBasisSideIndex, BasisPtr coarserBasis, int coarserBasisSideIndex, unsigned vertexNodePermutation); // requires the sides to have the same topology
+
+  // h
+  const FieldContainer<double> &constrainedWeights(BasisPtr finerBasis, RefinementBranch refinements, BasisPtr coarserBasis); // requires these to be defined on the same topology
+  const SubBasisReconciliationWeights &constrainedWeights(BasisPtr finerBasis, int finerBasisSideIndex, RefinementBranch refinements, BasisPtr coarserBasis, int coarserBasisSideIndex, unsigned vertexNodePermutation); // vertexPermutation is for the fine basis's ancestral orientation (how to permute side as seen by fine's ancestor to produce side as seen by coarse)...
   
   // static workhorse methods:
   
   /* Unbroken elements: */
   static FieldContainer<double> computeConstrainedWeights(BasisPtr finerBasis, BasisPtr coarserBasis); // requires these to be defined on the same topology
-  static SubBasisReconciliationWeights computeConstrainedWeights(BasisPtr finerBasis, BasisPtr coarserBasis, int finerBasisSideIndex, int coarserBasisSideIndex, unsigned vertexNodePermutation); // requires the sides to have the same topology
+  static SubBasisReconciliationWeights computeConstrainedWeights(BasisPtr finerBasis, int finerBasisSideIndex, BasisPtr coarserBasis, int coarserBasisSideIndex, unsigned vertexNodePermutation); // requires the sides to have the same topology
   // vertexNodePermutation: how to permute side as seen by finerBasis to produce side seen by coarserBasis.  Specifically, if iota_1 maps finerBasis vertices \hat{v}_i^1 to v_i in physical space, and iota_2 does the same for vertices of coarserBasis's topology, then the permutation is the one corresponding to iota_2^(-1) \ocirc iota_1.
   // vertexNodePermutation is an index into a structure defined by CellTopologyTraits.  See CellTopology::getNodePermutation() and CellTopology::getNodePermutationInverse().
   
   /* Broken elements: */
   // matching the whole bases:
-  static FieldContainer<double> computeConstrainedWeights(BasisPtr finerBasis, BasisPtr coarserBasis, const FieldContainer<double> &finerBasisElementNodesInCoarserBasisReferenceCell);
+  static FieldContainer<double> computeConstrainedWeights(BasisPtr finerBasis, RefinementBranch refinements, BasisPtr coarserBasis);
   // matching along sides:
-  static FieldContainer<double> computeConstrainedWeights(BasisPtr finerBasis, BasisPtr coarserBasis, int finerBasisSideIndex, int coarserBasisSideIndex,
-                                                          const FieldContainer<double> &finerBasisSideNodesInCoarserBasisReferenceCell);
+  static SubBasisReconciliationWeights computeConstrainedWeights(BasisPtr finerBasis, int finerBasisSideIndex, RefinementBranch refinements,
+                                                                 BasisPtr coarserBasis, int coarserBasisSideIndex, unsigned vertexNodePermutation);
   // it's worth noting that these FieldContainer arguments are not especially susceptible to caching
-  // for the caching interface, may want to use vector< pair< RefinementPattern*, int childIndex > > to define the coarse element's neighbor refinement hierarchy.  Probably still need a vertexPermutation
-  // as well to fully define the relationship.  Something like:
-private:
-  typedef vector< pair< RefinementPattern*, int > > RefinementHierarchy;
-  typedef pair< pair< Camellia::Basis<>*, Camellia::Basis<>* >, RefinementHierarchy > RefinedBasisPair;
-  typedef pair< pair< SideBasisRestriction, SideBasisRestriction >, RefinementHierarchy > SideRefinedBasisPair;
-  map< RefinedBasisPair, FieldContainer<double> > _simpleReconcilationWeights_h;
-  map< pair< SideRefinedBasisPair, unsigned> , FieldContainer<double> > _sideReconcilationWeights_h;  // unsigned: the vertexPermutation
 };
 
 /* a few ideas come up here:
