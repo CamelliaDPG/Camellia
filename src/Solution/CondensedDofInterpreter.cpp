1738bc6aed7a6dafe4891f573c75ac85c1517068
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-02-13 11:32:44 -0600

    Added CondensedDofInterpreter.

--- src/Solution/CondensedDofInterpreter.cpp
+++ src/Solution/CondensedDofInterpreter.cpp
@@ -7,3 +7,21 @@
 //
 
 #include "CondensedDofInterpreter.h"
+
+CondensedDofInterpreter::CondensedDofInterpreter(Mesh* mesh, LagrangeConstraints* lagrangeConstraints, const set<int> &fieldIDsToExclude, bool storeLocalStiffnessMatrices) {
+  _mesh = mesh;
+  _lagrangeConstraints = lagrangeConstraints;
+  _storeLocalStiffnessMatrices = storeLocalStiffnessMatrices;
+  _uncondensableVarIDs.insert(fieldIDsToExclude.begin(),fieldIDsToExclude.end());
+  
+}
+
+void CondensedDofInterpreter::interpretLocalData(GlobalIndexType cellID, const FieldContainer<double> &localDofs,
+                                                 FieldContainer<double> &globalDofs, FieldContainer<GlobalIndexType> &globalDofIndices) {
+  if (_storeLocalStiffnessMatrices && (localDofs.rank() == 2)) {
+    _localStiffnessMatrices[cellID] = localDofs;
+  }
+}
+void CondensedDofInterpreter::interpretGlobalData(GlobalIndexType cellID, FieldContainer<double> &localDofs, const Epetra_Vector &globalDofs) {
+  
+}
--- src/include/CondensedDofInterpreter.h
+++ src/include/CondensedDofInterpreter.h
@@ -9,6 +9,27 @@
 #ifndef Camellia_debug_CondensedDofInterpreter_h
 #define Camellia_debug_CondensedDofInterpreter_h
 
+#include "DofInterpreter.h"
+#include "Intrepid_FieldContainer.hpp"
+#include "Epetra_Vector.h"
+#include "Mesh.h"
+#include "LagrangeConstraints.h"
+
+using namespace Intrepid;
+
+class CondensedDofInterpreter : public DofInterpreter {
+  bool _storeLocalStiffnessMatrices;
+  Mesh* _mesh; // for element type lookup, and for determination of which dofs are trace dofs
+  LagrangeConstraints* _lagrangeConstraints;
+  set<int> _uncondensableVarIDs;
+  map<GlobalIndexType, FieldContainer<double> > _localStiffnessMatrices; // will be used by interpretGlobalData if _storeLocalStiffnessMatrices is true
+public:
+  CondensedDofInterpreter(Mesh* mesh, LagrangeConstraints* lagrangeConstraints, const set<int> &fieldIDsToExclude, bool storeLocalStiffnessMatrices);
+  
+  void interpretLocalData(GlobalIndexType cellID, const FieldContainer<double> &localDofs,
+                          FieldContainer<double> &globalDofs, FieldContainer<GlobalIndexType> &globalDofIndices);
+  void interpretGlobalData(GlobalIndexType cellID, FieldContainer<double> &localDofs, const Epetra_Vector &globalDofs);
+};
 
 
 #endif
