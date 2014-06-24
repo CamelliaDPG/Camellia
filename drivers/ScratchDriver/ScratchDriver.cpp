965ac93aabf563236194d44301983c4cfc01c5be
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-06-24 13:51:54 -0500

    Made the data in the sort int test for ScratchDriver exactly match the data we get prior to a crash on vesta, in the hope that I can isolate the issue on vesta.  It's a bit of a stretch, granted.

--- drivers/ScratchDriver/ScratchDriver.cpp
+++ drivers/ScratchDriver/ScratchDriver.cpp
@@ -329,15 +329,12 @@ int main(int argc, char *argv[]) {
   }
 
   if (trySortInt) {
-    int nvals = 3;
+    int nvals = 48;
     int *procs_from_ = new int[nvals];
     int *lengths_from_ = new int[nvals];
-    procs_from_[0] = 17;
-    procs_from_[1] = 16;
-    procs_from_[2] = 18;
-    lengths_from_[0] = 88;
-    lengths_from_[1] = 42;
-    lengths_from_[2] = 15;
+    procs_from_[0] = 21; procs_from_[1] = 23; procs_from_[2] = 29; procs_from_[3] = 17; procs_from_[4] = 20; procs_from_[5] = 22; procs_from_[6] = 19; procs_from_[7] = 28; procs_from_[8] = 16; procs_from_[9] = 25; procs_from_[10] = 18; procs_from_[11] = 30; procs_from_[12] = 27; procs_from_[13] = 24; procs_from_[14] = 26; procs_from_[15] = 5; procs_from_[16] = 7; procs_from_[17] = 4; procs_from_[18] = 13; procs_from_[19] = 1; procs_from_[20] = 15; procs_from_[21] = 3; procs_from_[22] = 6; procs_from_[23] = 12; procs_from_[24] = 2; procs_from_[25] = 0; procs_from_[26] = 11; procs_from_[27] = 9; procs_from_[28] = 14; procs_from_[29] = 8; procs_from_[30] = 10; procs_from_[31] = 43; procs_from_[32] = 40; procs_from_[33] = 42; procs_from_[34] = 34; procs_from_[35] = 41; procs_from_[36] = 46; procs_from_[37] = 35; procs_from_[38] = 38; procs_from_[39] = 44; procs_from_[40] = 39; procs_from_[41] = 32; procs_from_[42] = 45; procs_from_[43] = 33; procs_from_[44] = 36; procs_from_[45] = 37; procs_from_[46] = 47; procs_from_[47] = 31;
+    lengths_from_[0] = 683; lengths_from_[1] = 683; lengths_from_[2] = 683; lengths_from_[3] = 683; lengths_from_[4] = 683; lengths_from_[5] = 683; lengths_from_[6] = 683; lengths_from_[7] = 683; lengths_from_[8] = 683; lengths_from_[9] = 683; lengths_from_[10] = 683; lengths_from_[11] = 518; lengths_from_[12] = 683; lengths_from_[13] = 683; lengths_from_[14] = 683; lengths_from_[15] = 683; lengths_from_[16] = 683; lengths_from_[17] = 683; lengths_from_[18] = 683; lengths_from_[19] = 683; lengths_from_[20] = 683; lengths_from_[21] = 683; lengths_from_[22] = 683; lengths_from_[23] = 683; lengths_from_[24] = 683; lengths_from_[25] = 683; lengths_from_[26] = 683; lengths_from_[27] = 683; lengths_from_[28] = 683; lengths_from_[29] = 683; lengths_from_[30] = 683; lengths_from_[31] = 683; lengths_from_[32] = 683; lengths_from_[33] = 683; lengths_from_[34] = 683; lengths_from_[35] = 683; lengths_from_[36] = 683; lengths_from_[37] = 683; lengths_from_[38] = 683; lengths_from_[39] = 683; lengths_from_[40] = 683; lengths_from_[41] = 683; lengths_from_[42] = 683; lengths_from_[43] = 683; lengths_from_[44] = 683; lengths_from_[45] = 683; lengths_from_[46] = 683; lengths_from_[47] = 165;
+
     Sort_ints_(procs_from_, lengths_from_, nvals);
     cout << "After sorting";
     for (int i=0; i<nvals; i++) {
