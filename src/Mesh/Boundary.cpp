d168f3e5072e4a1937b4dabe327fb4cf04f86896
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-07-16 11:34:05 -0500

    Fixed an issue in the singleton BC impsosition related to a changing API for GDA.  (This will be committed as part of the distributed solution thing.)  Reverted to using the old API in Boundary for now.

--- src/Mesh/Boundary.cpp
+++ src/Mesh/Boundary.cpp
@@ -444,7 +444,8 @@ void Boundary::bcsToImpose( map<  GlobalIndexType, double > &globalDofIndicesAnd
               FieldContainer<double> globalCoefficients; // we'll ignore this
               FieldContainer<GlobalIndexType> globalDofIndices;
               _mesh->interpretLocalBasisCoefficients(cellID, trialID, sideOrdinal, basisCoefficients, globalCoefficients, globalDofIndices);
-              set<GlobalIndexType> rankLocalDofIndices = _mesh->globalDofAssignment()->globalDofIndicesForPartition(-1); // current rank
+              vector<GlobalIndexType> rankLocalDofIndicesVector = _mesh->globalDofAssignment()->globalDofIndicesForPartition(-1); // current rank
+              set<GlobalIndexType> rankLocalDofIndices(rankLocalDofIndices.begin(),rankLocalDofIndices.end());
               for (int i=0; i<globalDofIndices.size(); i++) {
                 if (rankLocalDofIndices.find(globalDofIndices[i]) != rankLocalDofIndices.end()) {
                   globalDofIndicesAndValues[globalDofIndices[i]] = 0.0;
