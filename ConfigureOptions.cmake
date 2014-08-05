f97b6a5a1950a93249346c1b14463b056937501d
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-08-05 10:42:27 -0500

    Unified the build (so far, tested with DPGTests; works there).

--- ConfigureOptions.cmake
+++ ConfigureOptions.cmake
@@ -1,14 +1,14 @@
-# Configure file for Camellia
+# Configure file for Camellia -- deprecated.  Suggestion is to use a do-configure script to specify any arguments from the cmake invocation.  This allows having debug/release/MPI/serial builds that all depend on the same copy of the Camellia source.
 
-set(TRILINOS_PATH /workspace/truman/trilinos)
-set(XDMF_PATH /workspace/truman/xdmf)
-set(HDF5_PATH /workspace/truman/hdf5)
+#set(TRILINOS_PATH /workspace/truman/trilinos)
+#set(XDMF_PATH /workspace/truman/xdmf)
+#set(HDF5_PATH /workspace/truman/hdf5)
 #set(MUMPS_PATH /opt/apps/ossw/libraries/mumps)
 #set(TRILINOS_PATH $ENV{TRILINOS_DIR})
 #set(MUMPS_PATH /workspace/jchan/MUMPS_4.9.2/)
 #set(SCALAPACK_PATH /workspace/jchan/lib/scalapack)
 
-set(CMAKE_CXX_COMPILER mpicxx)
-set(CMAKE_BUILD_TYPE DEBUG)
-set(BUILD_SHARED_LIBS ON)
-set(CMAKE_VERBOSE_MAKEFILE ON)
+#set(CMAKE_CXX_COMPILER mpicxx)
+#set(CMAKE_BUILD_TYPE DEBUG)
+#set(BUILD_SHARED_LIBS ON)
+#set(CMAKE_VERBOSE_MAKEFILE ON)
