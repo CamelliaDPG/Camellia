1d337fb2d8d593927702bac37f49721633a45c6a
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-08-19 14:30:44 -0500

    CMakeLists formatting.

---
+++ src/Mesh/GDAMinimumRuleConstraints.cpp
@@ -0,0 +1,9 @@
+//
+//  GDAMinimumRuleConstraints.cpp
+//  Camellia
+//
+//  Created by Nate Roberts on 8/18/14.
+//
+//
+
+#include "GDAMinimumRuleConstraints.h"
---
+++ src/include/GDAMinimumRuleConstraints.h
@@ -0,0 +1,14 @@
+//
+//  GDAMinimumRuleConstraints.h
+//  Camellia
+//
+//  Created by Nate Roberts on 8/18/14.
+//
+//
+
+#ifndef Camellia_GDAMinimumRuleConstraints_h
+#define Camellia_GDAMinimumRuleConstraints_h
+
+
+
+#endif
