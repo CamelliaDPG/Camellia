8ad82f268aba359281aa9286d159d67b49f9a191
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-10-14 12:57:36 -0500

    Added prototype IPFilter, which allows the definition of a LocalStiffnessMatrixFilter via a trial-space IP object.  Notion is that you might want to minimize (e_i, e_j)_E + (e_i, e_j)_ip, where E is the energy norm, and _ip is the inner product defined by the IP object.  Could be used for resolution of the extra constant mode in Stokes operator, e.g., if _ip were defined as (p,p)_L^2, say.

--- src/Solution/IPFilter.cpp
+++ src/Solution/IPFilter.cpp
@@ -7,3 +7,25 @@
 //
 
 #include "IPFilter.h"
+
+IPFilter::IPFilter(IPPtr ip) {
+  _ip = ip;
+}
+
+void IPFilter::filter(FieldContainer<double> &localStiffnessMatrix, FieldContainer<double> &localRHSVector,
+                      BasisCachePtr basisCache, Teuchos::RCP<Mesh> mesh, Teuchos::RCP<BC> bc) {
+  Teuchos::Array<int> dim(localStiffnessMatrix.rank());
+  for (int rank=0; rank<dim.size(); rank++) {
+    dim[rank] = localStiffnessMatrix.dimension(rank);
+  }
+  FieldContainer<double> ipMatrix(dim);
+  GlobalIndexType sampleCellID = basisCache->cellIDs()[0];
+  ElementTypePtr elemType = mesh->getElementType(sampleCellID);
+  DofOrderingPtr dofOrdering = elemType->trialOrderPtr;
+  _ip->computeInnerProductMatrix(ipMatrix, dofOrdering, basisCache);
+  
+  // add to localStiffnessMatrix
+  for (int i=0; i<localStiffnessMatrix.size(); i++) {
+    localStiffnessMatrix[i] += ipMatrix[i];
+  }
+}
--- src/include/IPFilter.h
+++ src/include/IPFilter.h
@@ -9,6 +9,15 @@
 #ifndef Camellia_IPFilter_h
 #define Camellia_IPFilter_h
 
+#include "IP.h"
+#include "LocalStiffnessMatrixFilter.h"
 
+class IPFilter : public LocalStiffnessMatrixFilter {
+  IPPtr _ip;
+public:
+  IPFilter(IPPtr ip); // idea is to use an IP on the trial space to add L^2 penalty terms.  In the case of Stokes I want integral of p = 0, so I might use an ip defined as (p,p).
+  void filter(FieldContainer<double> &localStiffnessMatrix, FieldContainer<double> &localRHSVector,
+              BasisCachePtr basisCache, Teuchos::RCP<Mesh> mesh, Teuchos::RCP<BC> bc);
+};
 
 #endif
