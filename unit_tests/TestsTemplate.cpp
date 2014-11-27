0676d7b367d27c148d13e4cc072e26b4e3e55a3d
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-11-26 23:15:19 -0600

    Added TestsTemplate.

---
+++ unit_tests/TestsTemplate.cpp
@@ -0,0 +1,24 @@
+//
+//  TestTemplate
+//  Camellia
+//
+//  Created by Nate Roberts on 11/25/14.
+//
+//
+
+// empty test file.  Copy (naming "MyClassTests.cpp", typically) and then add your tests below.
+
+#include "Teuchos_UnitTestHarness.hpp"
+namespace {
+//  TEUCHOS_UNIT_TEST( Int, Basic )
+//  {
+//    int i1 = 5;
+//    TEST_EQUALITY_CONST( i1, 5 );
+//  }
+//  TEUCHOS_UNIT_TEST( Int, Assignment )
+//  {
+//    int i1 = 4;
+//    int i2 = i1;
+//    TEST_EQUALITY( i2, i1 );
+//  }
+} // namespace
