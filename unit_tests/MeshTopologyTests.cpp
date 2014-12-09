6759ec26f4119d7027128519c89afe32bff3bd99
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-12-09 12:46:00 -0600

    Added test against initial mesh entities having appropriate active cell counts.

--- unit_tests/MeshTopologyTests.cpp
+++ unit_tests/MeshTopologyTests.cpp
@@ -14,6 +14,64 @@
 #include "MeshFactory.h"
 
 namespace {
+  TEUCHOS_UNIT_TEST( MeshTopology, InitialMeshEntitiesActiveCellCount)
+  {
+    // one easy way to create a quad mesh topology is to use MeshFactory
+    
+    int spaceDim = 2;
+    bool conformingTraces = false;
+    PoissonFormulation formulation(spaceDim, conformingTraces);
+    BFPtr bf = formulation.bf();
+    
+    int H1Order = 1;
+    
+    int horizontalElements = 1;
+    int verticalElements = 1;
+    double width = 1.0, height = 1.0;
+    MeshPtr mesh = MeshFactory::quadMesh(bf, H1Order, width, height, horizontalElements, verticalElements); // creates a 1-cell mesh
+    MeshTopologyPtr meshTopo = mesh->getTopology();
+  
+    unsigned sideDim = meshTopo->getSpaceDim() - 1;
+    
+    // uniform refinement --> none of the parent cells sides should have any active cells
+    CellPtr cell = meshTopo->getCell(0);
+    int sideCount = cell->getSideCount();
+    for (int sideOrdinal=0; sideOrdinal<sideCount; sideOrdinal++) {
+      IndexType sideEntityIndex = cell->entityIndex(sideDim, sideOrdinal);
+      
+      vector< pair<unsigned,unsigned> > activeCellInfoForSide = meshTopo->getActiveCellIndices(sideDim, sideEntityIndex);
+      
+      TEST_EQUALITY(activeCellInfoForSide.size(), 1);
+      TEST_EQUALITY(meshTopo->getActiveCellCount(sideDim, sideEntityIndex), activeCellInfoForSide.size());
+    }
+    
+    int vertexDim = 0;
+    int vertexCount = cell->topology()->getNodeCount();
+    for (int vertexOrdinal=0; vertexOrdinal<vertexCount; vertexOrdinal++) {
+      IndexType vertexEntityIndex = cell->entityIndex(vertexDim, vertexOrdinal);
+      
+      vector< pair<unsigned,unsigned> > activeCellInfoForVertex = meshTopo->getActiveCellIndices(vertexDim, vertexEntityIndex);
+      
+      TEST_EQUALITY(activeCellInfoForVertex.size(), 1);
+      TEST_EQUALITY(meshTopo->getActiveCellCount(vertexDim, vertexEntityIndex), activeCellInfoForVertex.size());
+    }
+    
+    // repeat, but now with a 2x1 mesh:
+    horizontalElements = 2;
+    verticalElements = 1;
+    mesh = MeshFactory::quadMesh(bf, H1Order, width, height, horizontalElements, verticalElements); // creates a 1-cell mesh
+    meshTopo = mesh->getTopology();
+    
+    for (int d=0; d<sideDim; d++) {
+      int entityCount = meshTopo->getEntityCount(d);
+      for (IndexType entityIndex=0; entityIndex < entityCount; entityIndex++) {
+        vector< pair<unsigned,unsigned> > activeCellInfoForEntity = meshTopo->getActiveCellIndices(d, entityIndex);
+        TEST_ASSERT((activeCellInfoForEntity.size() == 1) || (activeCellInfoForEntity.size() == 2));
+        TEST_EQUALITY(meshTopo->getActiveCellCount(d, entityIndex), activeCellInfoForEntity.size());
+      }
+    }
+  }
+  
   TEUCHOS_UNIT_TEST( MeshTopology, DeactivateCellOnRefinement)
   {
     // one easy way to create a quad mesh topology is to use MeshFactory
@@ -81,6 +139,5 @@ namespace {
         TEST_EQUALITY(constrainingSideAncestry.size(), 0);
       }
     }
-    
   }
 } // namespace
