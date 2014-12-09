3705e7f2ef49a58156d7f68a7eb62e5f5635bf65
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2014-12-09 17:34:08 -0600

    Added basic test of CamelliaCellTools::permutedReferenceCellPoints().

---
+++ unit_tests/CamelliaCellToolsTests.cpp
@@ -0,0 +1,48 @@
+//
+//  CamelliaCellToolsTests.cpp
+//  Camellia
+//
+//  Created by Nate Roberts on 11/18/14.
+//
+//
+
+#include "Teuchos_UnitTestHarness.hpp"
+#include "Teuchos_UnitTestHelpers.hpp"
+
+#include "Intrepid_FieldContainer.hpp"
+
+#include "Shards_CellTopology.hpp"
+
+#include "CamelliaCellTools.h"
+
+namespace {
+  TEUCHOS_UNIT_TEST( CamelliaCellTools, PermutedReferenceCellPoints )
+  {
+    // to begin, just a very simple test that *nodes* are permuted appropriately
+    
+    shards::CellTopology quad_4(shards::getCellTopologyData<shards::Quadrilateral<4> >() );
+
+    FieldContainer<double> refPoints(quad_4.getNodeCount(),quad_4.getDimension());
+    FieldContainer<double> quadNodesPermuted(quad_4.getNodeCount(),quad_4.getDimension());
+    
+    int permutationCount = quad_4.getNodePermutationCount();
+    
+    CamelliaCellTools::refCellNodesForTopology(refPoints, quad_4);
+    
+    FieldContainer<double> permutedRefPoints(quad_4.getNodeCount(),quad_4.getDimension());
+    
+    for (int permutation=0; permutation<permutationCount; permutation++) {
+      CamelliaCellTools::refCellNodesForTopology(quadNodesPermuted, quad_4, permutation);
+      
+      CamelliaCellTools::permutedReferenceCellPoints(quad_4, permutation, refPoints, permutedRefPoints);
+      
+      // expect permutedRefPoints = quadNodesPermuted
+      
+      for (int nodeOrdinal=0; nodeOrdinal < quadNodesPermuted.dimension(0); nodeOrdinal++) {
+        for (int d=0; d<quadNodesPermuted.dimension(1); d++) {
+          TEST_FLOATING_EQUALITY(quadNodesPermuted(nodeOrdinal,d), permutedRefPoints(nodeOrdinal,d), 1e-15);
+        }
+      }
+    }
+  }
+} // namespace
