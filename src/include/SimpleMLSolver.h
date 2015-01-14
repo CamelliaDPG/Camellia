df79a78ffa156fb68baae1181fd4f928401742b8
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-01-14 17:03:26 -0600

    Moved SimpleMLSolver implementation out of header file into cpp and required saveFactorization to be false (investigating issues on BG/Q).

--- src/include/SimpleMLSolver.h
+++ src/include/SimpleMLSolver.h
@@ -26,58 +26,10 @@ class SimpleMLSolver : public Solver {
   double _resTol;
   int _maxIters;
 public:
-  SimpleMLSolver(bool saveFactorization, double residualTolerance, int maxIterations) {
-    _saveFactorization = saveFactorization;
-    _resTol = residualTolerance;
-    _maxIters = maxIterations;
-
-  }
-  int solve() {
-    AztecOO *solver = new AztecOO( problem() );
-    // create a parameter list for ML options
-    ParameterList MLList;
-    
-    // Sets default parameters for classic smoothed aggregation. After this
-    // call, MLList contains the default values for the ML parameters,
-    // as required by typical smoothed aggregation for symmetric systems.
-    // Other sets of parameters are available for non-symmetric systems
-    // ("DD" and "DD-ML"), and for the Maxwell equations ("maxwell").
-    
-    int maxLevels = 8;
-    char parameter[80];
-    
-    ML_Epetra::MultiLevelPreconditioner* MLPrec;
-
-    ML_Epetra::SetDefaults("SA",MLList);
-  
-    Epetra_RowMatrix *A = problem().GetMatrix();
-    
-    MLPrec = new ML_Epetra::MultiLevelPreconditioner(*A, MLList);
-
-    solver->SetPrecOperator(MLPrec);
-    //    solver.SetAztecOption(AZ_solver, AZ_GMRESR); // could do AZ_cg -- we are SPD (but so far it seems cg takes a bit longer...)
-    solver->SetAztecOption(AZ_output, 0);
-    int result = solver->Iterate(_maxIters, _resTol);
-    
-    if (! _saveFactorization) {
-      // destroy the preconditioner
-      delete MLPrec;
-      // destroy the solver object
-      delete solver;
-    } else {
-      _savedSolver = Teuchos::rcp( solver );
-      _savedPreconditioner = Teuchos::rcp( MLPrec );
-    }
-    
-    return result;
-  }
-  int resolve() {
-    if (_savedSolver.get() != NULL) {
-      return _savedSolver->Iterate(_maxIters, _resTol);;
-    } else {
-      return solve();
-    }
-  }
+  SimpleMLSolver(bool saveFactorization, double residualTolerance, int maxIterations);
+  int solve();
+  int resolve();
+  void setProblem(Teuchos::RCP< Epetra_LinearProblem > problem);
 };
 
 #endif
