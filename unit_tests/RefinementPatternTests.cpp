d4458b217b17251afe942745a9b92a2f35e07052
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-01-29 17:32:28 -0600

    Added RefinementPattern tests MapSubcellFromChildToParent_CommonVertices, MapSubcellFromChildToParent_LineMiddleVertex.

--- unit_tests/RefinementPatternTests.cpp
+++ unit_tests/RefinementPatternTests.cpp
@@ -28,6 +28,93 @@ namespace {
     //  shardsTopologies.push_back(CellTopology::tetrahedron()); // tetrahedron not yet supported by permutation
     return shardsTopologies;
   }
+
+  TEUCHOS_UNIT_TEST( RefinementPattern, MapSubcellFromChildToParent_CommonVertices )
+  {
+    // test checks that regular refinement patterns do the right thing mapping from each child vertex to the parent,
+    // in the case of vertices that are shared by parent.
+    
+    vector< CellTopoPtr > shardsTopologies = getShardsTopologies();
+    
+    for (int topoOrdinal = 0; topoOrdinal < shardsTopologies.size(); topoOrdinal++) {
+      CellTopoPtr shardsTopo = shardsTopologies[topoOrdinal];
+      if (shardsTopo->getDimension() == 0) continue; // skip the point topology
+      RefinementPatternPtr refPattern = RefinementPattern::regularRefinementPattern(shardsTopo);
+      
+      FieldContainer<double> childVertices = refPattern->refinedNodes();
+      FieldContainer<double> parentVertices(shardsTopo->getVertexCount(),shardsTopo->getDimension());
+      CamelliaCellTools::refCellNodesForTopology(parentVertices, shardsTopo);
+      
+      double tol = 1e-15; // for vertex coordinate equality
+      
+      unsigned vertexDim = 0;
+      for (int childOrdinal=0; childOrdinal<childVertices.dimension(0); childOrdinal++) {
+        CellTopoPtr childTopo = refPattern->childTopology(childOrdinal);
+        for (int childVertexOrdinal = 0; childVertexOrdinal < childTopo->getVertexCount(); childVertexOrdinal++) {
+          int parentVertexOrdinal = -1;
+          // see if this vertex is present in parent:
+          for (int vertexOrdinal=0; vertexOrdinal<shardsTopo->getVertexCount(); vertexOrdinal++) {
+            bool matches = true;
+            for (int d=0; d < shardsTopo->getDimension(); d++) {
+              if (abs(childVertices(childOrdinal,childVertexOrdinal,d)-parentVertices(vertexOrdinal,d)) > tol) {
+                matches = false;
+              }
+            }
+            if (matches) {
+              parentVertexOrdinal = vertexOrdinal;
+              break; // we've found our match
+            }
+          }
+          if (parentVertexOrdinal == -1) continue; // no match in parent: we don't test this vertex
+          pair<unsigned,unsigned> vertexAncestor = refPattern->mapSubcellFromChildToParent(childOrdinal, vertexDim, childVertexOrdinal);
+          
+          TEST_EQUALITY(vertexAncestor.first, vertexDim);
+          TEST_EQUALITY(vertexAncestor.second, parentVertexOrdinal);
+        }
+      }
+    }
+  }
+  
+  TEUCHOS_UNIT_TEST( RefinementPattern, MapSubcellFromChildToParent_LineMiddleVertex)
+  {
+    // for the case of the line refinement pattern, tests the vertex we don't test in MapSubcellFromChildToParent_CommonVertices
+    
+    CellTopoPtr shardsTopo = CellTopology::line();
+    RefinementPatternPtr refPattern = RefinementPattern::regularRefinementPattern(shardsTopo);
+    
+    FieldContainer<double> childVertices = refPattern->refinedNodes();
+    FieldContainer<double> parentVertices(shardsTopo->getVertexCount(),shardsTopo->getDimension());
+    CamelliaCellTools::refCellNodesForTopology(parentVertices, shardsTopo);
+    
+    double tol = 1e-15; // for vertex coordinate equality
+    
+    unsigned vertexDim = 0, lineDim = 1;
+    unsigned parentLineOrdinal = 0;
+    for (int childOrdinal=0; childOrdinal<childVertices.dimension(0); childOrdinal++) {
+      CellTopoPtr childTopo = refPattern->childTopology(childOrdinal);
+      for (int childVertexOrdinal = 0; childVertexOrdinal < childTopo->getVertexCount(); childVertexOrdinal++) {
+        int parentVertexOrdinal = -1;
+        // see if this vertex is present in parent:
+        for (int vertexOrdinal=0; vertexOrdinal<shardsTopo->getVertexCount(); vertexOrdinal++) {
+          bool matches = true;
+          for (int d=0; d < shardsTopo->getDimension(); d++) {
+            if (abs(childVertices(childOrdinal,childVertexOrdinal,d)-parentVertices(vertexOrdinal,d)) > tol) {
+              matches = false;
+            }
+          }
+          if (matches) {
+            parentVertexOrdinal = vertexOrdinal;
+            break; // we've found our match
+          }
+        }
+        if (parentVertexOrdinal != -1) continue; // found match in parent: this is a vertex we test above
+        pair<unsigned,unsigned> vertexAncestor = refPattern->mapSubcellFromChildToParent(childOrdinal, vertexDim, childVertexOrdinal);
+        
+        TEST_EQUALITY(vertexAncestor.first, lineDim);
+        TEST_EQUALITY(vertexAncestor.second, parentLineOrdinal);
+      }
+    }
+  }
   
   TEUCHOS_UNIT_TEST( RefinementPattern, MapRefCellPointsToAncestor_ChildNodeIdentity )
   {
