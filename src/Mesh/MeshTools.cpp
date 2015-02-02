e6339fb154367337351b5dceb5a7de8c62170835
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-02-02 13:43:50 -0600

    Added new partition policy for time slice meshes, a policy induced from the mesh of which the slice is taken.

--- src/Mesh/MeshTools.cpp
+++ src/Mesh/MeshTools.cpp
@@ -19,6 +19,42 @@
 #include "HDF5Exporter.h"
 #endif
 
+#include "MeshPartitionPolicy.h"
+
+class InducedMeshPartitionPolicy : public MeshPartitionPolicy {
+  // (note that the induced partition policy will break if either mesh is refined, since the cellID map will change...)
+  
+  map<GlobalIndexType, GlobalIndexType> _cellIDMap; // keys are this mesh's cellIDs; values are otherMesh's
+  MeshPtr _otherMesh;
+public:
+  InducedMeshPartitionPolicy(MeshPtr otherMesh, const map<GlobalIndexType, GlobalIndexType> & cellIDMap) {
+    _otherMesh = otherMesh;
+    _cellIDMap = cellIDMap; // copy
+  }
+  
+  virtual void partitionMesh(Mesh *mesh, PartitionIndexType numPartitions) {
+    int otherPartitionCount = _otherMesh->globalDofAssignment()->getPartitionCount();
+    if (numPartitions < otherPartitionCount) {
+      TEUCHOS_TEST_FOR_EXCEPTION(true, std::invalid_argument, "Induced partition count must be greater than or equal to otherMesh's");
+    }
+    
+    set<GlobalIndexType> activeCellIDs = mesh->getActiveCellIDs();
+    vector< set<GlobalIndexType> > partitions(numPartitions);
+    
+    for (set<GlobalIndexType>::iterator myCellIDIt = activeCellIDs.begin(); myCellIDIt != activeCellIDs.end(); myCellIDIt++) {
+      GlobalIndexType myCellID = *myCellIDIt;
+      if (_cellIDMap.find(myCellID) == _cellIDMap.end()) {
+        TEUCHOS_TEST_FOR_EXCEPTION(true, std::invalid_argument, "cellID not found in _cellIDMap");
+      }
+      GlobalIndexType otherCellID = _cellIDMap[myCellID];
+      int otherPartitionNumber = _otherMesh->globalDofAssignment()->partitionForCellID(otherCellID);
+      partitions[otherPartitionNumber].insert(myCellID);
+    }
+    
+    mesh->globalDofAssignment()->setPartitions(partitions);
+  }
+};
+
 // time slice utilities assume a tensor product cell in the time dimension
 // and further assume that if there are nodeCount vertices in the spatial element, then the nodes in space-time
 // will be ordered such that nodes(i) and nodes(i+nodeCount) correspond to each other
@@ -128,6 +164,10 @@ MeshPtr MeshTools::timeSliceMesh(MeshPtr spaceTimeMesh, double t,
     }
   }
   
+  MeshPartitionPolicyPtr partitionPolicy = Teuchos::rcp( new InducedMeshPartitionPolicy(spaceTimeMesh, sliceCellIDToSpaceTimeCellID) );
+  
+  sliceMesh->setPartitionPolicy(partitionPolicy);
+  
   return sliceMesh;
 }
 
@@ -137,7 +177,8 @@ class SliceFunction : public Function {
   FunctionPtr _spaceTimeFunction;
   double _t;
 public:
-  SliceFunction(MeshPtr spaceTimeMesh, map<GlobalIndexType, GlobalIndexType> &cellIDMap, FunctionPtr spaceTimeFunction, double t) : Function(spaceTimeFunction->rank()) {
+  SliceFunction(MeshPtr spaceTimeMesh, map<GlobalIndexType, GlobalIndexType> &cellIDMap,
+                FunctionPtr spaceTimeFunction, double t) : Function(spaceTimeFunction->rank()) {
     _spaceTimeMesh = spaceTimeMesh;
     _cellIDMap = cellIDMap;
     _spaceTimeFunction = spaceTimeFunction;
