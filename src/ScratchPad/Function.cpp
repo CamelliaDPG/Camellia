c757aefa126c9a31b90ee8bb7ef5cd991f2523ee
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-03-02 14:02:13 -0600

    Added support in SimpleSolutionFunction for searching for cells that match physical points provided.

--- src/ScratchPad/Function.cpp
+++ src/ScratchPad/Function.cpp
@@ -2616,7 +2616,52 @@ void SimpleSolutionFunction::importCellData(std::vector<GlobalIndexType> cells) 
 
 void SimpleSolutionFunction::values(FieldContainer<double> &values, BasisCachePtr basisCache) {
   bool dontWeightForCubature = false;
-  _soln->solutionValues(values, _var->ID(), basisCache, dontWeightForCubature, _var->op());
+  if (basisCache->mesh().get() != NULL) { // then we assume that the BasisCache is appropriate for solution's mesh...
+    _soln->solutionValues(values, _var->ID(), basisCache, dontWeightForCubature, _var->op());
+  } else {
+    // the following adapted from PreviousSolutionFunction.  Probably would do well to consolidate
+    // that class with this one at some point...
+    LinearTermPtr solnExpression = 1.0 * _var;
+    // get the physicalPoints, and make a basisCache for each...
+    FieldContainer<double> physicalPoints = basisCache->getPhysicalCubaturePoints();
+    FieldContainer<double> value(1,1); // assumes scalar-valued solution function.
+    int numCells = physicalPoints.dimension(0);
+    int numPoints = physicalPoints.dimension(1);
+    int spaceDim = physicalPoints.dimension(2);
+    physicalPoints.resize(numCells*numPoints,spaceDim);
+    vector< ElementPtr > elements = _soln->mesh()->elementsForPoints(physicalPoints, false); // false: don't make elements null just because they're off-rank.
+    FieldContainer<double> point(1,1,spaceDim);
+    FieldContainer<double> refPoint(1,spaceDim);
+    int combinedIndex = 0;
+    vector<GlobalIndexType> cellID;
+    cellID.push_back(-1);
+    BasisCachePtr basisCacheOnePoint;
+    for (int cellIndex=0; cellIndex<numCells; cellIndex++) {
+      for (int ptIndex=0; ptIndex<numPoints; ptIndex++, combinedIndex++) {
+        if (elements[combinedIndex].get()==NULL) continue; // no element found for point; skip it…
+        ElementTypePtr elemType = elements[combinedIndex]->elementType();
+        for (int d=0; d<spaceDim; d++) {
+          point(0,0,d) = physicalPoints(combinedIndex,d);
+        }
+        if (elements[combinedIndex]->cellID() != cellID[0]) {
+          cellID[0] = elements[combinedIndex]->cellID();
+          basisCacheOnePoint = Teuchos::rcp( new BasisCache(elemType, _soln->mesh()) );
+          basisCacheOnePoint->setPhysicalCellNodes(_soln->mesh()->physicalCellNodesForCell(cellID[0]),cellID,false); // false: don't createSideCacheToo
+        }
+        refPoint.resize(1,1,spaceDim); // CamelliaCellTools::mapToReferenceFrame wants a numCells dimension...  (perhaps it shouldn't, though!)
+        // compute the refPoint:
+        CamelliaCellTools::mapToReferenceFrame(refPoint,point,_soln->mesh()->getTopology(), cellID[0],
+                                               _soln->mesh()->globalDofAssignment()->getCubatureDegree(cellID[0]));
+        refPoint.resize(1,spaceDim);
+        basisCacheOnePoint->setRefCellPoints(refPoint);
+        //          cout << "refCellPoints:\n " << refPoint;
+        //          cout << "physicalCubaturePoints:\n " << basisCacheOnePoint->getPhysicalCubaturePoints();
+        solnExpression->evaluate(value, _soln, basisCacheOnePoint);
+        //          cout << "value at point (" << point(0,0) << ", " << point(0,1) << ") = " << value(0,0) << endl;
+        values(cellIndex,ptIndex) = value(0,0);
+      }
+    }
+  }
   if (_var->varType()==FLUX) { // weight by sideParity
     sideParity()->scalarMultiplyFunctionValues(values, basisCache);
   }
