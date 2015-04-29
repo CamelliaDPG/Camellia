5581b948b2bb14730490913894b4ac74598dc71e
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-04-29 17:10:50 -0500

    Added several tests to SpaceTimeHeatFormulationTests, centered around BC enforcement and actually doing a solve.  These fail.

--- unit_tests/SpaceTimeHeatFormulationTests.cpp
+++ unit_tests/SpaceTimeHeatFormulationTests.cpp
@@ -8,6 +8,7 @@
 
 #include "Teuchos_UnitTestHarness.hpp"
 
+#include "Boundary.h"
 #include "HDF5Exporter.h"
 #include "MeshFactory.h"
 #include "SpaceTimeHeatFormulation.h"
@@ -94,6 +95,62 @@ namespace {
     TEST_COMPARE(l2_diff, <, 1e-14);
   }
 
+  void testSpaceTimeHeatConsistency(int spaceDim, Teuchos::FancyOStream &out, bool &success)
+  {
+    vector<double> dimensions(spaceDim,2.0); // 2.0^d hypercube domain
+    vector<int> elementCounts(spaceDim,1);   // 1^d mesh
+    vector<double> x0(spaceDim,-1.0);
+    MeshTopologyPtr spatialMeshTopo = MeshFactory::rectilinearMeshTopology(dimensions, elementCounts, x0);
+    
+    double t0 = 0.0, t1 = 1.0;
+    MeshTopologyPtr spaceTimeMeshTopo = MeshFactory::spaceTimeMeshTopology(spatialMeshTopo, t0, t1);
+    
+    double epsilon = 0.1;
+    int fieldPolyOrder = 2, delta_k = 1;
+    
+    FunctionPtr u;
+    FunctionPtr x = Function::xn(1);
+    FunctionPtr y = Function::yn(1);
+    FunctionPtr z = Function::zn(1);
+    FunctionPtr t = Function::tn(1);
+    
+    if (spaceDim == 1)
+    {
+      u = x * t;
+    }
+    else if (spaceDim == 2)
+    {
+      u = x * t + y;
+    }
+    else if (spaceDim == 3)
+    {
+      u = x * t + y - z;
+    }
+    
+    bool useConformingTraces = true;
+    SpaceTimeHeatFormulation form(spaceDim, useConformingTraces, epsilon);
+    
+    setupExactSolution(form, u, spaceTimeMeshTopo, fieldPolyOrder, delta_k);
+    projectExactSolution(form, form.solution(), u);
+    
+    form.solution()->clearComputedResiduals();
+    
+    double energyError = form.solution()->energyErrorTotal();
+    
+//    if (spaceDim != 3)
+//    {
+//      MeshPtr mesh = form.solution()->mesh();
+//      string outputDir = "/tmp";
+//      string solnName = (spaceDim == 1) ? "spaceTimeHeatSolution_1D" : "spaceTimeHeatSolution_2D";
+//      cout << "\nDebugging: Outputting spaceTimeHeatSolution to " << outputDir << "/" << solnName << endl;
+//      HDF5Exporter exporter(mesh, solnName, outputDir);
+//      exporter.exportSolution(form.solution());
+//    }
+    
+    double tol = 1e-13;
+    TEST_COMPARE(energyError, <, tol);
+  }
+  
   void testSpaceTimeHeatConsistencyConstantSolution(int spaceDim, Teuchos::FancyOStream &out, bool &success)
   {
     vector<double> dimensions(spaceDim,2.0); // 2.0^d hypercube domain
@@ -119,76 +176,135 @@ namespace {
 
     double energyError = form.solution()->energyErrorTotal();
 
-//    if (spaceDim != 3)
-//    {
-//      MeshPtr mesh = form.solution()->mesh();
-//      string outputDir = "/tmp";
-//      string solnName = (spaceDim == 1) ? "spaceTimeHeatConstantSolution_1D" : "spaceTimeHeatConstantSolution_2D";
-//      cout << "\nDebugging: Outputting spaceTimeHeatSolution to " << outputDir << "/" << solnName << endl;
-//      HDF5Exporter exporter(mesh, solnName, outputDir);
-//      exporter.exportSolution(form.solution());
-//    }
-
     double tol = 1e-13;
     TEST_COMPARE(energyError, <, tol);
   }
 
-  void testSpaceTimeHeatConsistency(int spaceDim, Teuchos::FancyOStream &out, bool &success)
+  void testSpaceTimeHeatImposeConstantTraceBCs(int spaceDim, Teuchos::FancyOStream &out, bool &success)
   {
     vector<double> dimensions(spaceDim,2.0); // 2.0^d hypercube domain
-    vector<int> elementCounts(spaceDim,1);   // 1^d mesh
+    vector<int> elementCounts(spaceDim,1);   // one-element mesh
     vector<double> x0(spaceDim,-1.0);
     MeshTopologyPtr spatialMeshTopo = MeshFactory::rectilinearMeshTopology(dimensions, elementCounts, x0);
-
+    
     double t0 = 0.0, t1 = 1.0;
     MeshTopologyPtr spaceTimeMeshTopo = MeshFactory::spaceTimeMeshTopology(spatialMeshTopo, t0, t1);
-
-    double epsilon = 0.1;
-    int fieldPolyOrder = 2, delta_k = 1;
-
-    FunctionPtr u;
-    FunctionPtr x = Function::xn(1);
-    FunctionPtr y = Function::yn(1);
-    FunctionPtr z = Function::zn(1);
-    FunctionPtr t = Function::tn(1);
-
-    if (spaceDim == 1)
-    {
-      u = x * t;
-    }
-    else if (spaceDim == 2)
-    {
-      u = x * t + y;
-    }
-    else if (spaceDim == 3)
-    {
-      u = x * t + y - z;
-    }
-
+    
+    double epsilon = .1;
+    int fieldPolyOrder = 1, delta_k = 1;
+    
+    static const double CONST_VALUE = 0.5;
+    FunctionPtr u = Function::constant(CONST_VALUE);
+    
     bool useConformingTraces = true;
     SpaceTimeHeatFormulation form(spaceDim, useConformingTraces, epsilon);
-
+    
     setupExactSolution(form, u, spaceTimeMeshTopo, fieldPolyOrder, delta_k);
-    projectExactSolution(form, form.solution(), u);
-
-    form.solution()->clearComputedResiduals();
-
+    
+    VarPtr u_hat = form.u_hat();
+    bool isTrace = true;
+    BCPtr bc = form.solution()->bc();
+    bc->addDirichlet(u_hat, SpatialFilter::allSpace(), u);
+    
+    MeshPtr mesh = form.solution()->mesh();
+    
+    Boundary boundary = mesh->boundary();
+    DofInterpreter* dofInterpreter = form.solution()->getDofInterpreter().get();
+    std::map<GlobalIndexType, double> globalDofIndicesAndValues;
+    GlobalIndexType cellID = 0;
+    set<pair<int, unsigned>> singletons;
+    boundary.bcsToImpose<double>(globalDofIndicesAndValues, *bc, cellID, singletons, dofInterpreter, NULL);
+    
+    // use our knowledge that we have a one-element mesh: every last dof for u_hat should be present, and have coefficient CONST_VALUE
+    DofOrderingPtr trialOrder = mesh->getElementType(cellID)->trialOrderPtr;
+    CellTopoPtr cellTopo = mesh->getElementType(cellID)->cellTopoPtr;
+    
+    BasisCachePtr basisCache = BasisCache::basisCacheForCell(mesh, cellID);
+    
+    double tol = 1e-13;
+    for (int sideOrdinal=0; sideOrdinal < cellTopo->getSideCount(); sideOrdinal++)
+    {
+      out << "******** SIDE " << sideOrdinal << " ********" << endl;
+      BasisPtr basis = trialOrder->getBasis(u_hat->ID(),sideOrdinal);
+      Intrepid::FieldContainer<double> uValues(basis->getCardinality());
+      uValues.initialize(CONST_VALUE);
+      Intrepid::FieldContainer<double> globalData;
+      Intrepid::FieldContainer<GlobalIndexType> globalDofIndices;
+      dofInterpreter->interpretLocalBasisCoefficients(cellID, u_hat->ID(), sideOrdinal, uValues, globalData, globalDofIndices);
+      // sanity check on the interpreted global values
+      for (int basisOrdinal=0; basisOrdinal<globalData.size(); basisOrdinal++)
+      {
+        TEST_FLOATING_EQUALITY(CONST_VALUE, globalData(basisOrdinal), tol);
+      }
+      
+      for (int basisOrdinal=0; basisOrdinal<globalData.size(); basisOrdinal++)
+      {
+        GlobalIndexType globalDofIndex = globalDofIndices(basisOrdinal);
+        if (globalDofIndicesAndValues.find(globalDofIndex) != globalDofIndicesAndValues.end())
+        {
+          double expectedValue = globalData(basisOrdinal);
+          double actualValue = globalDofIndicesAndValues[globalDofIndex];
+          TEST_FLOATING_EQUALITY(expectedValue, actualValue, tol);
+        }
+        else
+        {
+          out << "On side " << sideOrdinal << ", did not find globalDofIndex " << globalDofIndex << endl;
+          success = false;
+        }
+      }
+    }
+  }
+  
+  void testSpaceTimeHeatSolveConstantSolution(int spaceDim, bool useTraceBCsEverywhere, Teuchos::FancyOStream &out, bool &success)
+  {
+    vector<double> dimensions(spaceDim,2.0); // 2.0^d hypercube domain
+    vector<int> elementCounts(spaceDim,1);   // one-element mesh
+    vector<double> x0(spaceDim,-1.0);
+    MeshTopologyPtr spatialMeshTopo = MeshFactory::rectilinearMeshTopology(dimensions, elementCounts, x0);
+    
+    double t0 = 0.0, t1 = 1.0;
+    MeshTopologyPtr spaceTimeMeshTopo = MeshFactory::spaceTimeMeshTopology(spatialMeshTopo, t0, t1);
+    
+    double epsilon = .1;
+    int fieldPolyOrder = 1, delta_k = 1;
+    
+    FunctionPtr u = Function::constant(0.5);
+    
+    bool useConformingTraces = true;
+    SpaceTimeHeatFormulation form(spaceDim, useConformingTraces, epsilon);
+    
+    setupExactSolution(form, u, spaceTimeMeshTopo, fieldPolyOrder, delta_k);
+    
+    if (!useTraceBCsEverywhere)
+    {
+      out << "useTraceBCsEverywhere = false not yet supported/implemented in test.\n";
+      success = false;
+    }
+    else
+    {
+      VarPtr u_hat = form.u_hat();
+      BCPtr bc = form.solution()->bc();
+      bc->addDirichlet(u_hat, SpatialFilter::allSpace(), u);
+    }
+    
+    form.solution()->solve();
+    
+    if (spaceDim != 3)
+    {
+      MeshPtr mesh = form.solution()->mesh();
+      string outputDir = "/tmp";
+      string solnName = (spaceDim == 1) ? "spaceTimeHeatSolution_1D" : "spaceTimeHeatSolution_2D";
+      cout << "\nDebugging: Outputting spaceTimeHeatSolution to " << outputDir << "/" << solnName << endl;
+      HDF5Exporter exporter(mesh, solnName, outputDir);
+      exporter.exportSolution(form.solution());
+    }
+    
     double energyError = form.solution()->energyErrorTotal();
-
-//    if (spaceDim != 3)
-//    {
-//      MeshPtr mesh = form.solution()->mesh();
-//      string outputDir = "/tmp";
-//      string solnName = (spaceDim == 1) ? "spaceTimeHeatSolution_1D" : "spaceTimeHeatSolution_2D";
-//      cout << "\nDebugging: Outputting spaceTimeHeatSolution to " << outputDir << "/" << solnName << endl;
-//      HDF5Exporter exporter(mesh, solnName, outputDir);
-//      exporter.exportSolution(form.solution());
-//    }
-
+    
     double tol = 1e-13;
     TEST_COMPARE(energyError, <, tol);
   }
-
+  
   TEUCHOS_UNIT_TEST( SpaceTimeHeatFormulation, ConsistencyConstantSolution_1D )
   {
     // consistency test for space-time formulation with 1D space
@@ -241,5 +357,18 @@ namespace {
   {
     // constant u should imply forcing function is zero
     testForcingFunctionForConstantU(3, out, success);
+  }
+
+  TEUCHOS_UNIT_TEST( SpaceTimeHeatFormulation, ImposeConstantTraceBCs_1D )
+  {
+    // test BC imposition for space-time formulation with 1D space, exact solution with u constant
+    testSpaceTimeHeatImposeConstantTraceBCs(1, out, success);
+  }
+  
+  TEUCHOS_UNIT_TEST( SpaceTimeHeatFormulation, SolveConstantSolution_1D )
+  {
+    // test solve for space-time formulation with 1D space, exact solution with u constant
+    bool useTraceBCsEverywhere = true;
+    testSpaceTimeHeatSolveConstantSolution(1, useTraceBCsEverywhere, out, success);
   }
 } // namespace
