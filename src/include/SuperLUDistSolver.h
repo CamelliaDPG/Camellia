7ce52ed6118e08c415a71a71e98e26347e107cb8
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-06-10 12:50:57 -0500

    Added setting "ReuseSymbolic" and "Fact" parameters when saveFactorization is true.

--- src/include/SuperLUDistSolver.h
+++ src/include/SuperLUDistSolver.h
@@ -14,6 +14,8 @@
 #ifdef HAVE_AMESOS_SUPERLUDIST
 #include "Amesos_Superludist.h"
 
+#include "MPIWrapper.h"
+
 namespace Camellia {
 
   class SuperLUDistSolver : public Solver
@@ -21,24 +23,43 @@ namespace Camellia {
     Teuchos::RCP<Amesos_Superludist> _savedSolver;
     Teuchos::RCP<Epetra_LinearProblem> _savedProblem;
     bool _saveFactorization;
+    bool _havePrintedStatus;
   public:
     SuperLUDistSolver(bool saveFactorization)
     {
       _saveFactorization = saveFactorization;
+      _havePrintedStatus = false;
     }
     
     int solve() {
+      Teuchos::ParameterList paramList;
+      
       if (!_saveFactorization)
       {
         Epetra_LinearProblem problem(this->_stiffnessMatrix.get(), this->_lhs.get(), this->_rhs.get());
         Amesos_Superludist slu(problem);
+        slu.SetParameters(paramList);
         return slu.Solve();
       }
       else
       {
         _savedProblem = Teuchos::rcp( new Epetra_LinearProblem(this->_stiffnessMatrix.get(), this->_lhs.get(), this->_rhs.get()) );
         _savedSolver = Teuchos::rcp( new Amesos_Superludist(*_savedProblem) );
-        return _savedSolver->Solve();
+        
+        paramList.set("ReuseSymbolic",true);
+        paramList.set("Fact", "SamePattern_SameRowPerm");
+        _savedSolver->SetParameters(paramList);
+        
+        int err = _savedSolver->Solve();
+        
+        if (!_havePrintedStatus)
+        { // print status for diagnostic purposes
+          if (MPIWrapper::rank() == 0) cout << "SuperLUDistSolver.h : SuperLU_Dist factorization reuse requested.  Details of solver:\n";
+          _savedSolver->PrintStatus();
+          _havePrintedStatus = true;
+        }
+        
+        return err;
       }
     }
     
@@ -56,6 +77,7 @@ namespace Camellia {
     {
       _savedSolver = Teuchos::null;
       _savedProblem = Teuchos::null;
+      _havePrintedStatus = false;
     }
   };
 }
