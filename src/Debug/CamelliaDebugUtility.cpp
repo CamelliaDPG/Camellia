6b4b5dd896bb91da6f1d8650d6624693513b1da0
Nathan V. Roberts <nvroberts@alcf.anl.gov>
2015-06-10 11:30:02 -0500

    Added printMapSummary() to CamelliaDebugUtility for printing very brief summary of Epetra_Map on a rank.

--- src/Debug/CamelliaDebugUtility.cpp
+++ src/Debug/CamelliaDebugUtility.cpp
@@ -8,8 +8,9 @@
 
 #include "CamelliaDebugUtility.h"
 
-#include "VarFactory.h"
 #include "DofOrdering.h"
+#include "MPIWrapper.h"
+#include "VarFactory.h"
 
 #include <iostream>
 
@@ -18,150 +19,125 @@ using namespace std;
 using namespace Intrepid;
 namespace Camellia
 {
-
-template<typename data_type>
-void print(string name, vector<data_type> &data)
-{
-  cout << name << ": ";
-  for (int i=0; i<data.size(); i++)
+  
+  template<typename data_type>
+  void print(string name, vector<data_type> &data)
   {
-    cout << data[i] << " ";
-  }
-  cout << endl;
-}
-
-template<typename data_type>
-void print(string name, set<data_type> &data)
-{
-  cout << name << ": ";
-  for (typename set<data_type>::iterator dataIt=data.begin(); dataIt != data.end(); dataIt++)
-  {
-    cout << *dataIt << " ";
-  }
-  cout << endl;
-}
-
-template<typename key_type, typename value_type>
-void print(string name, map<key_type, value_type> &data)
-{
-  cout << name << ": ";
-  for (typename map<key_type, value_type>::iterator dataIt=data.begin(); dataIt != data.end(); dataIt++)
-  {
-    cout << "(" << dataIt->first << " => " << dataIt->second << ") ";
-  }
-  cout << endl;
-}
-
-void print(string name, map<int, double> data)
-{
-  print<int, double>(name, data);
-}
-
-void print(string name, map<unsigned, double> data)
-{
-  print<unsigned, double>(name, data);
-}
-
-void print(string name, map<unsigned, unsigned> data)
-{
-  print<unsigned, unsigned>(name, data);
-}
-
-void print(string name, vector<long long> data)
-{
-  print<long long>(name, data);
-}
-
-void print(string name, vector<int> data)
-{
-  print<int>(name, data);
-}
-void print(string name, vector<unsigned> data)
-{
-  print<unsigned>(name, data);
-}
-void print(string name, vector<double> data)
-{
-  print<double>(name,data);
-}
-void print(string name, set<unsigned> data)
-{
-  print<unsigned>(name, data);
-}
-void print(string name, set<int> data)
-{
-  print<int>(name, data);
-}
-void print(string name, set<long long> data)
-{
-  print<long long>(name, data);
-}
-void print(string name, set<double> data)
-{
-  print<double>(name, data);
-}
-
-void printLabeledDofCoefficients(VarFactoryPtr vf, DofOrderingPtr dofOrdering,
-                                 const Intrepid::FieldContainer<double> &dofCoefficients,
-                                 bool trialSpaceDofs)
-{
-  if (dofOrdering->totalDofs() != dofCoefficients.size())
-  {
-    TEUCHOS_TEST_FOR_EXCEPTION(true,std::invalid_argument,"dofOrdering size does not match dofCoefficients container size");
-  }
-
-  if (!trialSpaceDofs)
-  {
-    TEUCHOS_TEST_FOR_EXCEPTION(true,std::invalid_argument,"printLabeledCoefficients does not yet support test variables");
-  }
-
-  Teuchos::oblackholestream oldFormatState;
-  oldFormatState.copyfmt(cout);
-
-  int myprec = cout.precision();
-  cout.setf(std::ios_base::scientific, std::ios_base::floatfield);
-  cout.setf(std::ios_base::right);
-
-  vector< VarPtr > fieldVars = vf->fieldVars();
-  vector< VarPtr > traceVars = vf->traceVars();
-  vector< VarPtr > fluxVars = vf->fluxVars();
-
-  bool printNonZerosOnly = true;
-  if (printNonZerosOnly) cout << "*** (printing only nonzeros) *** \n";
-
-  if (fieldVars.size() > 0)
-  {
-    cout << "\n\n **************   FIELD coefficients   **************\n";
-    for (vector<VarPtr>::iterator varIt = fieldVars.begin(); varIt != fieldVars.end(); varIt++)
+    cout << name << ": ";
+    for (int i=0; i<data.size(); i++)
     {
-      VarPtr var = *varIt;
-      cout << var->displayString() << ":\n";
-      const std::vector<int>* dofIndices = &dofOrdering->getDofIndices(var->ID());
-      int basisDofOrdinal = 0;
-      for (std::vector<int>::const_iterator dofIndexIt = dofIndices->begin(); dofIndexIt != dofIndices->end(); dofIndexIt++, basisDofOrdinal++)
-      {
-        if (printNonZerosOnly && (dofCoefficients[*dofIndexIt]==0.0)) continue;
-        cout.setf(std::ios::right, std::ios::adjustfield);
-        cout << std::setw(3) << basisDofOrdinal;
-        cout << "             ";
-        cout.setf(std::ios::right, std::ios::adjustfield);
-        cout << std::setw(myprec+8) << dofCoefficients[*dofIndexIt] << "\n";
-      }
+      cout << data[i] << " ";
     }
+    cout << endl;
   }
-
-  if (traceVars.size() > 0)
+  
+  template<typename data_type>
+  void print(string name, set<data_type> &data)
   {
-    cout << "\n\n ***************   TRACE coefficients   ***************\n";
-    for (vector<VarPtr>::iterator varIt = traceVars.begin(); varIt != traceVars.end(); varIt++)
+    cout << name << ": ";
+    for (typename set<data_type>::iterator dataIt=data.begin(); dataIt != data.end(); dataIt++)
     {
-      VarPtr var = *varIt;
-      const vector<int>* sides = &dofOrdering->getSidesForVarID(var->ID());
-      for (vector<int>::const_iterator sideIt = sides->begin(); sideIt != sides->end(); sideIt++)
+      cout << *dataIt << " ";
+    }
+    cout << endl;
+  }
+  
+  template<typename key_type, typename value_type>
+  void print(string name, map<key_type, value_type> &data)
+  {
+    cout << name << ": ";
+    for (typename map<key_type, value_type>::iterator dataIt=data.begin(); dataIt != data.end(); dataIt++)
+    {
+      cout << "(" << dataIt->first << " => " << dataIt->second << ") ";
+    }
+    cout << endl;
+  }
+  
+  void print(string name, map<int, double> data)
+  {
+    print<int, double>(name, data);
+  }
+  
+  void print(string name, map<unsigned, double> data)
+  {
+    print<unsigned, double>(name, data);
+  }
+  
+  void print(string name, map<unsigned, unsigned> data)
+  {
+    print<unsigned, unsigned>(name, data);
+  }
+  
+  void print(string name, vector<long long> data)
+  {
+    print<long long>(name, data);
+  }
+  
+  void print(string name, vector<int> data)
+  {
+    print<int>(name, data);
+  }
+  void print(string name, vector<unsigned> data)
+  {
+    print<unsigned>(name, data);
+  }
+  void print(string name, vector<double> data)
+  {
+    print<double>(name,data);
+  }
+  void print(string name, set<unsigned> data)
+  {
+    print<unsigned>(name, data);
+  }
+  void print(string name, set<int> data)
+  {
+    print<int>(name, data);
+  }
+  void print(string name, set<long long> data)
+  {
+    print<long long>(name, data);
+  }
+  void print(string name, set<double> data)
+  {
+    print<double>(name, data);
+  }
+  
+  void printLabeledDofCoefficients(VarFactoryPtr vf, DofOrderingPtr dofOrdering,
+                                   const Intrepid::FieldContainer<double> &dofCoefficients,
+                                   bool trialSpaceDofs)
+  {
+    if (dofOrdering->totalDofs() != dofCoefficients.size())
+    {
+      TEUCHOS_TEST_FOR_EXCEPTION(true,std::invalid_argument,"dofOrdering size does not match dofCoefficients container size");
+    }
+    
+    if (!trialSpaceDofs)
+    {
+      TEUCHOS_TEST_FOR_EXCEPTION(true,std::invalid_argument,"printLabeledCoefficients does not yet support test variables");
+    }
+    
+    Teuchos::oblackholestream oldFormatState;
+    oldFormatState.copyfmt(cout);
+    
+    int myprec = cout.precision();
+    cout.setf(std::ios_base::scientific, std::ios_base::floatfield);
+    cout.setf(std::ios_base::right);
+    
+    vector< VarPtr > fieldVars = vf->fieldVars();
+    vector< VarPtr > traceVars = vf->traceVars();
+    vector< VarPtr > fluxVars = vf->fluxVars();
+    
+    bool printNonZerosOnly = true;
+    if (printNonZerosOnly) cout << "*** (printing only nonzeros) *** \n";
+    
+    if (fieldVars.size() > 0)
+    {
+      cout << "\n\n **************   FIELD coefficients   **************\n";
+      for (vector<VarPtr>::iterator varIt = fieldVars.begin(); varIt != fieldVars.end(); varIt++)
       {
-        int sideOrdinal = *sideIt;
-        cout << var->displayString() << ", side " << sideOrdinal << ":\n";
-        const std::vector<int>* dofIndices = &dofOrdering->getDofIndices(var->ID(), sideOrdinal);
+        VarPtr var = *varIt;
+        cout << var->displayString() << ":\n";
+        const std::vector<int>* dofIndices = &dofOrdering->getDofIndices(var->ID());
         int basisDofOrdinal = 0;
         for (std::vector<int>::const_iterator dofIndexIt = dofIndices->begin(); dofIndexIt != dofIndices->end(); dofIndexIt++, basisDofOrdinal++)
         {
@@ -174,34 +150,65 @@ void printLabeledDofCoefficients(VarFactoryPtr vf, DofOrderingPtr dofOrdering,
         }
       }
     }
-  }
-
-  if (fluxVars.size() > 0)
-  {
-    cout << "\n\n ***************   FLUX coefficients   ***************\n";
-    for (vector<VarPtr>::iterator varIt = fluxVars.begin(); varIt != fluxVars.end(); varIt++)
+    
+    if (traceVars.size() > 0)
     {
-      VarPtr var = *varIt;
-      const vector<int>* sides = &dofOrdering->getSidesForVarID(var->ID());
-      for (vector<int>::const_iterator sideIt = sides->begin(); sideIt != sides->end(); sideIt++)
+      cout << "\n\n ***************   TRACE coefficients   ***************\n";
+      for (vector<VarPtr>::iterator varIt = traceVars.begin(); varIt != traceVars.end(); varIt++)
       {
-        int sideOrdinal = *sideIt;
-        cout << var->displayString() << ", side " << sideOrdinal << ":\n";
-        const std::vector<int>* dofIndices = &dofOrdering->getDofIndices(var->ID(), sideOrdinal);
-        int basisDofOrdinal = 0;
-        for (std::vector<int>::const_iterator dofIndexIt = dofIndices->begin(); dofIndexIt != dofIndices->end(); dofIndexIt++, basisDofOrdinal++)
+        VarPtr var = *varIt;
+        const vector<int>* sides = &dofOrdering->getSidesForVarID(var->ID());
+        for (vector<int>::const_iterator sideIt = sides->begin(); sideIt != sides->end(); sideIt++)
         {
-          if (printNonZerosOnly && (dofCoefficients[*dofIndexIt]==0.0)) continue;
-          cout.setf(std::ios::right, std::ios::adjustfield);
-          cout << std::setw(3) << basisDofOrdinal;
-          cout << "             ";
-          cout.setf(std::ios::right, std::ios::adjustfield);
-          cout << std::setw(myprec+8) << dofCoefficients[*dofIndexIt] << "\n";
+          int sideOrdinal = *sideIt;
+          cout << var->displayString() << ", side " << sideOrdinal << ":\n";
+          const std::vector<int>* dofIndices = &dofOrdering->getDofIndices(var->ID(), sideOrdinal);
+          int basisDofOrdinal = 0;
+          for (std::vector<int>::const_iterator dofIndexIt = dofIndices->begin(); dofIndexIt != dofIndices->end(); dofIndexIt++, basisDofOrdinal++)
+          {
+            if (printNonZerosOnly && (dofCoefficients[*dofIndexIt]==0.0)) continue;
+            cout.setf(std::ios::right, std::ios::adjustfield);
+            cout << std::setw(3) << basisDofOrdinal;
+            cout << "             ";
+            cout.setf(std::ios::right, std::ios::adjustfield);
+            cout << std::setw(myprec+8) << dofCoefficients[*dofIndexIt] << "\n";
+          }
         }
       }
     }
+    
+    if (fluxVars.size() > 0)
+    {
+      cout << "\n\n ***************   FLUX coefficients   ***************\n";
+      for (vector<VarPtr>::iterator varIt = fluxVars.begin(); varIt != fluxVars.end(); varIt++)
+      {
+        VarPtr var = *varIt;
+        const vector<int>* sides = &dofOrdering->getSidesForVarID(var->ID());
+        for (vector<int>::const_iterator sideIt = sides->begin(); sideIt != sides->end(); sideIt++)
+        {
+          int sideOrdinal = *sideIt;
+          cout << var->displayString() << ", side " << sideOrdinal << ":\n";
+          const std::vector<int>* dofIndices = &dofOrdering->getDofIndices(var->ID(), sideOrdinal);
+          int basisDofOrdinal = 0;
+          for (std::vector<int>::const_iterator dofIndexIt = dofIndices->begin(); dofIndexIt != dofIndices->end(); dofIndexIt++, basisDofOrdinal++)
+          {
+            if (printNonZerosOnly && (dofCoefficients[*dofIndexIt]==0.0)) continue;
+            cout.setf(std::ios::right, std::ios::adjustfield);
+            cout << std::setw(3) << basisDofOrdinal;
+            cout << "             ";
+            cout.setf(std::ios::right, std::ios::adjustfield);
+            cout << std::setw(myprec+8) << dofCoefficients[*dofIndexIt] << "\n";
+          }
+        }
+      }
+    }
+    
+    cout.copyfmt(oldFormatState);
   }
-
-  cout.copyfmt(oldFormatState);
-}
+  
+  void printMapSummary(const Epetra_Map &map, string mapName = "map")
+  {
+    int rank = MPIWrapper::rank();
+    cout << "On rank " << rank << ", " << mapName << " has " << map.NumMyElements() << " of " << map.NumGlobalElements() << " global elements.\n";
+  }
 }
--- src/include/CamelliaDebugUtility.h
+++ src/include/CamelliaDebugUtility.h
@@ -8,37 +8,41 @@
 #include <map>
 #include <string>
 
+#include "Epetra_Map.h"
 #include "Teuchos_RCP.hpp"
 #include "Intrepid_FieldContainer.hpp"
 
 namespace Camellia
 {
-template<typename data_type>
-void print(std::string name, std::set<data_type> &data);
-
-template<typename key_type, typename value_type>
-void print(std::string name, std::map<key_type, value_type> &data);
-
-void print(std::string name, std::map<unsigned, unsigned> data);
-void print(std::string name, std::map<unsigned, double> data);
-void print(std::string name, std::map<int, double> data);
-
-void print(std::string name, std::vector<int> data);
-void print(std::string name, std::vector<unsigned> data);
-void print(std::string name, std::vector<long long> data);
-void print(std::string name, std::vector<double> data);
-
-void print(std::string name, std::set<unsigned> data);
-void print(std::string name, std::set<int> data);
-void print(std::string name, std::set<long long> data);
-void print(std::string name, std::set<double> data);
-
-// ! prints out the coefficients for each variable, labelled using the names from the VarFactory.
-// ! If the trialSpaceDofs boolean is set to true, interprets the variable IDs as trial space variables;
-// ! otherwise, interprets them as test space variables.
-void printLabeledDofCoefficients(VarFactoryPtr vf, DofOrderingPtr dofOrdering,
-                                 const Intrepid::FieldContainer<double> &dofCoefficients,
-                                 bool trialSpaceDofs = true);
+  template<typename data_type>
+  void print(std::string name, std::set<data_type> &data);
+  
+  template<typename key_type, typename value_type>
+  void print(std::string name, std::map<key_type, value_type> &data);
+  
+  void print(std::string name, std::map<unsigned, unsigned> data);
+  void print(std::string name, std::map<unsigned, double> data);
+  void print(std::string name, std::map<int, double> data);
+  
+  void print(std::string name, std::vector<int> data);
+  void print(std::string name, std::vector<unsigned> data);
+  void print(std::string name, std::vector<long long> data);
+  void print(std::string name, std::vector<double> data);
+  
+  void print(std::string name, std::set<unsigned> data);
+  void print(std::string name, std::set<int> data);
+  void print(std::string name, std::set<long long> data);
+  void print(std::string name, std::set<double> data);
+  
+  // ! prints out the coefficients for each variable, labelled using the names from the VarFactory.
+  // ! If the trialSpaceDofs boolean is set to true, interprets the variable IDs as trial space variables;
+  // ! otherwise, interprets them as test space variables.
+  void printLabeledDofCoefficients(VarFactoryPtr vf, DofOrderingPtr dofOrdering,
+                                   const Intrepid::FieldContainer<double> &dofCoefficients,
+                                   bool trialSpaceDofs = true);
+  
+  // ! Prints out a summary of the rank-local map information.  Not an MPI collective operation.
+  void printMapSummary(const Epetra_Map &map, std::string mapName);
 }
 
 #endif
