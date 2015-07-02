8ba25bed6cf9e4e405c241b9f4ed8b0834539ee2
Truman Ellis <ellis.truman@gmail.com>
2015-07-02 15:44:03 -0500

    Added compressible formulation, but not working yet

---
+++ drivers/Truman/Dissertation/TSpaceTimeCompressible.cpp
@@ -0,0 +1,296 @@
+#include "Solution.h"
+#include "RHS.h"
+
+#include "MeshUtilities.h"
+#include "MeshFactory.h"
+
+#include <Teuchos_GlobalMPISession.hpp>
+
+#include "Teuchos_CommandLineProcessor.hpp"
+#include "Teuchos_ParameterList.hpp"
+#include "Teuchos_TimeMonitor.hpp"
+#include "Amesos_config.h"
+
+#include "EpetraExt_ConfigDefs.h"
+#ifdef HAVE_EPETRAEXT_HDF5
+#include "HDF5Exporter.h"
+#endif
+
+#include "BF.h"
+#include "RefinementStrategy.h"
+#include "GMGSolver.h"
+#include "SpaceTimeCompressibleFormulation.h"
+#include "CompressibleProblems.h"
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+using namespace Camellia;
+
+int main(int argc, char *argv[])
+{
+
+#ifdef HAVE_MPI
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv,0);
+
+  Epetra_MpiComm Comm(MPI_COMM_WORLD);
+#else
+  Epetra_SerialComm Comm;
+#endif
+
+  int commRank = Teuchos::GlobalMPISession::getRank();
+
+  Comm.Barrier(); // set breakpoint here to allow debugger attachment to other MPI processes than the one you automatically attached to.
+
+  Teuchos::CommandLineProcessor cmdp(false,true); // false: don't throw exceptions; true: do return errors for unrecognized options
+
+  // problem parameters:
+  int spaceDim = 1;
+  double Re = 40;
+  bool steady = false;
+  string problemChoice = "TrivialCompressible";
+  int numRefs = 1;
+  int p = 2, delta_p = 2;
+  int numXElems = 1;
+  int numTElems = 1;
+  int numSlabs = 1;
+  bool useConformingTraces = false;
+  string solverChoice = "KLU";
+  double solverTolerance = 1e-8;
+  double nonlinearTolerance = 1e-5;
+  int maxLinearIterations = 1000;
+  int maxNonlinearIterations = 20;
+  bool computeL2Error = false;
+  bool exportSolution = false;
+  bool saveSolution = false;
+  bool loadSolution = false;
+  int loadRef = 0;
+  int loadDirRef = 0;
+  string norm = "Graph";
+  string rootDir = ".";
+  string tag="";
+  cmdp.setOption("spaceDim", &spaceDim, "spatial dimension");
+  cmdp.setOption("Re", &Re, "Re");
+  cmdp.setOption("steady", "transient", &steady, "use steady incompressible Navier-Stokes");
+  cmdp.setOption("problem", &problemChoice, "TrivialCompressible, TaylorGreen");
+  cmdp.setOption("polyOrder",&p,"polynomial order for field variable u");
+  cmdp.setOption("delta_p", &delta_p, "test space polynomial order enrichment");
+  cmdp.setOption("numRefs",&numRefs,"number of refinements");
+  cmdp.setOption("numXElems",&numXElems,"number of elements in x direction");
+  cmdp.setOption("numTElems",&numTElems,"number of elements in t direction");
+  cmdp.setOption("numSlabs",&numSlabs,"number of time slabs to use");
+  cmdp.setOption("norm", &norm, "norm");
+  cmdp.setOption("conformingTraces", "nonconformingTraces", &useConformingTraces, "use conforming traces");
+  cmdp.setOption("solver", &solverChoice, "KLU, SuperLU, MUMPS, GMG-Direct, GMG-ILU, GMG-IC");
+  cmdp.setOption("solverTolerance", &solverTolerance, "iterative solver tolerance");
+  cmdp.setOption("nonlinearTolerance", &nonlinearTolerance, "nonlinear solver tolerance");
+  cmdp.setOption("maxLinearIterations", &maxLinearIterations, "maximum number of iterations for linear solver");
+  cmdp.setOption("maxNonlinearIterations", &maxNonlinearIterations, "maximum number of iterations for Newton solver");
+  cmdp.setOption("outputDir", &rootDir, "output directory");
+  cmdp.setOption("computeL2Error", "skipL2Error", &computeL2Error, "compute L2 error");
+  cmdp.setOption("exportSolution", "skipExport", &exportSolution, "export solution to HDF5");
+  cmdp.setOption("saveSolution", "skipSave", &saveSolution, "save mesh and solution to HDF5");
+  cmdp.setOption("loadSolution", "skipLoad", &loadSolution, "load mesh and solution from HDF5");
+  cmdp.setOption("loadRef", &loadRef, "load refinement number");
+  cmdp.setOption("loadDirRef", &loadDirRef, "which refinement directory to load from");
+  cmdp.setOption("tag", &tag, "output tag");
+
+  if (cmdp.parse(argc,argv) != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL)
+  {
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+    return -1;
+  }
+
+  map<string, Teuchos::RCP<CompressibleProblem>> problems;
+  problems["TrivialCompressible"] = Teuchos::rcp(new TrivialCompressible(steady, Re));
+  // problems["TaylorGreen"] = Teuchos::rcp(new TaylorGreenProblem(steady, Re, numXElems, numSlabs));
+  // problems["Cylinder"] = Teuchos::rcp(new CylinderProblem(steady, Re, numSlabs));
+  // problems["SquareCylinder"] = Teuchos::rcp(new SquareCylinderProblem(steady, Re, numSlabs));
+  Teuchos::RCP<CompressibleProblem> problem = problems.at(problemChoice);
+
+  // if (commRank == 0)
+  // {
+  //   Solver::printAvailableSolversReport();
+  //   cout << endl;
+  // }
+  Teuchos::RCP<Time> totalTimer = Teuchos::TimeMonitor::getNewCounter("Total Time");
+  totalTimer->start(true);
+
+  for (; problem->currentStep() < problem->numSlabs(); problem->advanceStep())
+  {
+    if (problem->numSlabs() > 1 && commRank == 0 && !steady)
+      cout << "Solving time slab [" << problem->currentT0() << ", " << problem->currentT1() << "]" << endl;
+
+    ostringstream problemName;
+    string isSteady = "Steady";
+    if (!steady)
+      isSteady = "Transient";
+    problemName << isSteady << problemChoice << spaceDim << "D_slab" << problem->currentStep() << "_" << norm << "_" << Re << "_p" << p << "_" << solverChoice;
+    if (tag != "")
+      problemName << "_" << tag;
+    ostringstream saveDir;
+    saveDir << problemName.str() << "_ref" << loadRef;
+
+    int success = mkdir((rootDir+"/"+saveDir.str()).c_str(), S_IRWXU | S_IRWXG);
+
+    string dataFileLocation = rootDir + "/" + saveDir.str() + "/" + saveDir.str() + ".data";
+    string exportName = saveDir.str();
+
+    ostringstream loadDir;
+    loadDir << problemName.str() << "_ref" << loadDirRef;
+    string loadFilePrefix = "";
+    if (loadSolution)
+    {
+      loadFilePrefix = rootDir + "/" + loadDir.str() + "/" + saveDir.str();
+      if (commRank == 0) cout << "Loading previous solution " << loadFilePrefix << endl;
+    }
+    // ostringstream saveDir;
+    // saveDir << problemName.str() << "_ref" << loadRef;
+    string saveFilePrefix = rootDir + "/" + saveDir.str() + "/" + problemName.str();
+    if (saveSolution && commRank == 0) cout << "Saving to " << saveFilePrefix << endl;
+
+    Teuchos::ParameterList parameters;
+    parameters.set("spaceDim", spaceDim);
+    parameters.set("steady", steady);
+    parameters.set("mu", 1./Re);
+    parameters.set("useConformingTraces", useConformingTraces);
+    parameters.set("fieldPolyOrder", p);
+    parameters.set("delta_p", delta_p);
+    parameters.set("numTElems", numTElems);
+    parameters.set("norm", norm);
+    parameters.set("savedSolutionAndMeshPrefix", loadFilePrefix);
+    SpaceTimeCompressibleFormulationPtr form = Teuchos::rcp(new SpaceTimeCompressibleFormulation(problem, parameters));
+
+    MeshPtr mesh = form->solutionUpdate()->mesh();
+    MeshPtr k0Mesh = Teuchos::rcp( new Mesh (mesh->getTopology()->deepCopy(), form->bf(), 1, delta_p) );
+    mesh->registerObserver(k0Mesh);
+
+    // Set up boundary conditions
+    problem->setBCs(form);
+
+    // Set up solution
+    SolutionPtr solutionUpdate = form->solutionUpdate();
+    SolutionPtr solutionBackground = form->solutionBackground();
+    // dynamic_cast<AnalyticalCompressibleProblem*>(problem.get())->projectExactSolution(solutionBackground);
+
+    RefinementStrategyPtr refStrategy = form->getRefinementStrategy();
+    Teuchos::RCP<HDF5Exporter> exporter;
+    if (exportSolution)
+      exporter = Teuchos::rcp(new HDF5Exporter(mesh,exportName, rootDir));
+
+    Teuchos::RCP<Time> solverTime = Teuchos::TimeMonitor::getNewCounter("Solve Time");
+    map<string, SolverPtr> solvers;
+    solvers["KLU"] = Solver::getSolver(Solver::KLU, true);
+#if defined(HAVE_AMESOS_SUPERLUDIST) || defined(HAVE_AMESOS2_SUPERLUDIST)
+    solvers["SuperLUDist"] = Solver::getSolver(Solver::SuperLUDist, true);
+#endif
+#ifdef HAVE_AMESOS_MUMPS
+    solvers["MUMPS"] = Solver::getSolver(Solver::MUMPS, true);
+#endif
+    bool useStaticCondensation = false;
+    int azOutput = 20; // print residual every 20 CG iterations
+    ofstream dataFile(dataFileLocation);
+    dataFile << "ref\t " << "elements\t " << "dofs\t " << "energy\t " << "l2\t " << "solvetime\t" << "iterations\t " << endl;
+
+    // {
+    //   // ostringstream saveFile;
+    //   // saveFile << saveFilePrefix << "_ref" << -1;
+    //   // form->save(saveFile.str());
+    //   exporter->exportSolution(solutionBackground, -1);
+    //   if (commRank == 0)
+    //     cout << "Done exporting" << endl;
+    // }
+
+    for (int refIndex=loadRef; refIndex <= numRefs; refIndex++)
+    {
+      double l2Update = 1e10;
+      int iterCount = 0;
+      solverTime->start(true);
+      while (l2Update > nonlinearTolerance && iterCount < maxNonlinearIterations)
+      {
+        Teuchos::RCP<GMGSolver> gmgSolver;
+        if (solverChoice[0] == 'G')
+        {
+          gmgSolver = Teuchos::rcp( new GMGSolver(solutionUpdate, k0Mesh, maxLinearIterations, solverTolerance, Solver::getDirectSolver(true), useStaticCondensation));
+          gmgSolver->setAztecOutput(azOutput);
+          if (solverChoice == "GMG-Direct")
+            gmgSolver->gmgOperator()->setSchwarzFactorizationType(GMGOperator::Direct);
+          if (solverChoice == "GMG-ILU")
+            gmgSolver->gmgOperator()->setSchwarzFactorizationType(GMGOperator::ILU);
+          if (solverChoice == "GMG-IC")
+            gmgSolver->gmgOperator()->setSchwarzFactorizationType(GMGOperator::IC);
+          solutionUpdate->solve(gmgSolver);
+        }
+        else
+          solutionUpdate->condensedSolve(solvers[solverChoice]);
+
+        // Compute L2 norm of update
+        double rhoL2Update = solutionUpdate->L2NormOfSolutionGlobal(form->rho()->ID());
+        double u1L2Update = solutionUpdate->L2NormOfSolutionGlobal(form->u(1)->ID());
+        double u2L2Update = 0, u3L2Update = 0;
+        if (spaceDim > 1)
+          u2L2Update = solutionUpdate->L2NormOfSolutionGlobal(form->u(2)->ID());
+        if (spaceDim > 2)
+          u3L2Update = solutionUpdate->L2NormOfSolutionGlobal(form->u(3)->ID());
+        double TL2Update = solutionUpdate->L2NormOfSolutionGlobal(form->T()->ID());
+        l2Update = sqrt(rhoL2Update*rhoL2Update
+            + u1L2Update*u1L2Update + u2L2Update*u2L2Update + u3L2Update*u3L2Update
+            + TL2Update*TL2Update);
+        if (commRank == 0)
+          cout << "Nonlinear Update:\t " << l2Update << endl;
+
+        form->updateSolution();
+        iterCount++;
+      }
+      double solveTime = solverTime->stop();
+
+      double energyError = solutionUpdate->energyErrorTotal();
+      double l2Error = 0;
+      if (computeL2Error)
+      {
+        l2Error = problem->computeL2Error(form, solutionBackground);
+      }
+      if (commRank == 0)
+      {
+        cout << "Refinement: " << refIndex
+          << " \tElements: " << mesh->numActiveElements()
+          << " \tDOFs: " << mesh->numGlobalDofs()
+          << " \tEnergy Error: " << energyError
+          << " \tL2 Error: " << l2Error
+          << " \tSolve Time: " << solveTime
+          // << " \tIteration Count: " << iterationCount
+          << endl;
+        dataFile << refIndex
+          << " " << mesh->numActiveElements()
+          << " " << mesh->numGlobalDofs()
+          << " " << energyError
+          << " " << l2Error
+          << " " << solveTime
+          // << " " << iterationCount
+          << endl;
+      }
+
+      if (exportSolution)
+        exporter->exportSolution(solutionBackground, refIndex);
+
+      if (saveSolution)
+      {
+        ostringstream saveFile;
+        saveFile << saveFilePrefix << "_ref" << refIndex;
+        form->save(saveFile.str());
+      }
+
+      if (refIndex != numRefs)
+        refStrategy->refine();
+    }
+    dataFile.close();
+  }
+  double totalTime = totalTimer->stop();
+  if (commRank == 0)
+    cout << "Total time = " << totalTime << endl;
+
+  return 0;
+}
---
+++ src/include/CompressibleProblems.h
@@ -0,0 +1,238 @@
+
+#include "Function.h"
+#include "SpatialFilter.h"
+#include "ExpFunction.h"
+#include "TrigFunctions.h"
+#include "PenaltyConstraints.h"
+#include "SpaceTimeCompressibleFormulation.h"
+
+namespace Camellia
+{
+class CompressibleProblem
+{
+  protected:
+    Teuchos::RCP<PenaltyConstraints> _pc = Teuchos::null;
+    FunctionPtr _rho_exact;
+    FunctionPtr _u1_exact;
+    FunctionPtr _u2_exact;
+    FunctionPtr _u3_exact;
+    FunctionPtr _T_exact;
+    double _gamma = 1.4;
+    double _Pr = 0.713;
+    double _Cv;
+    double _Cp;
+    double _R;
+    double _tInit;
+    double _tFinal;
+    int _numSlabs = 1;
+    int _currentStep = 0;
+    bool _steady;
+  public:
+    double gamma() { return _gamma; }
+    double Pr() { return _Pr; }
+    double Cv() { return _Cv; }
+    double Cp() { return _Cp; }
+    double R() { return _R; }
+    LinearTermPtr forcingTerm = Teuchos::null;
+    FunctionPtr rho_exact() { return _rho_exact; }
+    FunctionPtr u1_exact() { return _u1_exact; }
+    FunctionPtr u2_exact() { return _u2_exact; }
+    FunctionPtr u3_exact() { return _u3_exact; }
+    FunctionPtr T_exact() { return _T_exact; }
+    virtual MeshTopologyPtr meshTopology(int temporalDivisions=1) = 0;
+    virtual MeshGeometryPtr meshGeometry() { return Teuchos::null; }
+    virtual void preprocessMesh(MeshPtr proxyMesh) {};
+    virtual void setBCs(SpaceTimeCompressibleFormulationPtr form) = 0;
+    Teuchos::RCP<PenaltyConstraints> pc() { return _pc; }
+    virtual double computeL2Error(SpaceTimeCompressibleFormulationPtr form, SolutionPtr solutionBackground) { return 0; }
+    int numSlabs() { return _numSlabs; }
+    int currentStep() { return _currentStep; }
+    void advanceStep() { _currentStep++; }
+    double stepSize() { return (_tFinal-_tInit)/_numSlabs; }
+    double currentT0() { return stepSize()*_currentStep; }
+    double currentT1() { return stepSize()*(_currentStep+1); }
+};
+
+class AnalyticalCompressibleProblem : public CompressibleProblem
+{
+  protected:
+    vector<double> _x0;
+    vector<double> _dimensions;
+    vector<int> _elementCounts;
+    map<int, FunctionPtr> _exactMap;
+  public:
+    virtual MeshTopologyPtr meshTopology(int temporalDivisions=1)
+    {
+      MeshTopologyPtr spatialMeshTopo = MeshFactory::rectilinearMeshTopology(_dimensions, _elementCounts, _x0);
+      MeshTopologyPtr spaceTimeMeshTopo = MeshFactory::spaceTimeMeshTopology(spatialMeshTopo, currentT0(), currentT1(), temporalDivisions);
+      if (_steady)
+        return spatialMeshTopo;
+      else
+        return spaceTimeMeshTopo;
+    }
+
+    void initializeExactMap(SpaceTimeCompressibleFormulationPtr form)
+    {
+      _exactMap[form->rho()->ID()] = _rho_exact;
+      _exactMap[form->T()->ID()] = _T_exact;
+      switch (form->spaceDim())
+      {
+        case 1:
+          _exactMap[form->u(1)->ID()] = _u1_exact;
+          _exactMap[form->uhat(1)->ID()] = form->uhat(1)->termTraced()->evaluate(_exactMap);
+          break;
+        case 2:
+          _exactMap[form->u(1)->ID()] = _u1_exact;
+          _exactMap[form->u(2)->ID()] = _u2_exact;
+          _exactMap[form->uhat(1)->ID()] = form->uhat(1)->termTraced()->evaluate(_exactMap);
+          _exactMap[form->uhat(2)->ID()] = form->uhat(2)->termTraced()->evaluate(_exactMap);
+          break;
+        case 3:
+          _exactMap[form->u(1)->ID()] = _u1_exact;
+          _exactMap[form->u(2)->ID()] = _u2_exact;
+          _exactMap[form->u(3)->ID()] = _u3_exact;
+          _exactMap[form->uhat(1)->ID()] = form->uhat(1)->termTraced()->evaluate(_exactMap);
+          _exactMap[form->uhat(2)->ID()] = form->uhat(2)->termTraced()->evaluate(_exactMap);
+          _exactMap[form->uhat(3)->ID()] = form->uhat(3)->termTraced()->evaluate(_exactMap);
+          break;
+      }
+    }
+
+    void projectExactSolution(SolutionPtr solution)
+    {
+      solution->projectOntoMesh(_exactMap);
+    }
+
+    virtual void setBCs(SpaceTimeCompressibleFormulationPtr form)
+    {
+      initializeExactMap(form);
+
+      BCPtr bc = form->solutionUpdate()->bc();
+      SpatialFilterPtr initTime = SpatialFilter::matchingT(_tInit);
+      SpatialFilterPtr leftX  = SpatialFilter::matchingX(_x0[0]);
+      SpatialFilterPtr rightX = SpatialFilter::matchingX(_x0[0]+_dimensions[0]);
+      SpatialFilterPtr leftY  = SpatialFilter::matchingY(_x0[1]);
+      SpatialFilterPtr rightY = SpatialFilter::matchingY(_x0[1]+_dimensions[1]);
+      FunctionPtr one = Function::constant(1);
+      switch (form->spaceDim())
+      {
+        case 1:
+          bc->addDirichlet(form->tc(),   leftX,  -_rho_exact*_u1_exact );
+          bc->addDirichlet(form->tc(),   rightX,  _rho_exact*_u1_exact );
+          bc->addDirichlet(form->tm(1),  leftX, -(_rho_exact*_u1_exact*_u1_exact+_R*_rho_exact*_T_exact));
+          bc->addDirichlet(form->tm(1),  rightX, (_rho_exact*_u1_exact*_u1_exact+_R*_rho_exact*_T_exact));
+          bc->addDirichlet(form->te(),   leftX, -(_rho_exact*_Cv*_T_exact+0.5*_rho_exact*_u1_exact*_u1_exact+_R*_rho_exact*_T_exact)*_u1_exact);
+          bc->addDirichlet(form->te(),   rightX, (_rho_exact*_Cv*_T_exact+0.5*_rho_exact*_u1_exact*_u1_exact+_R*_rho_exact*_T_exact)*_u1_exact);
+
+          // bc->addDirichlet(form->uhat(1), leftX,    _exactMap[form->uhat(1)->ID()]);
+          // bc->addDirichlet(form->uhat(2), leftX,    _exactMap[form->uhat(2)->ID()]);
+          // bc->addDirichlet(form->uhat(1), rightX,   _exactMap[form->uhat(1)->ID()]);
+          // bc->addDirichlet(form->uhat(2), rightX,   _exactMap[form->uhat(2)->ID()]);
+          // bc->addDirichlet(form->T(),     leftX,    _exactMap[form->T()->ID()]);
+          // bc->addDirichlet(form->T(),     rightX,   _exactMap[form->T()->ID()]);
+          if (!_steady)
+          {
+            FunctionPtr rho_init = _exactMap[form->rho()->ID()];
+            FunctionPtr u1_init  = _exactMap[form->u(1)->ID()];
+            FunctionPtr T_init   = _exactMap[form->T()->ID()];
+            FunctionPtr m1_init = rho_init*u1_init;
+            FunctionPtr E_init = rho_init*(_Cv*T_init + 0.5*u1_init*u1_init);
+            bc->addDirichlet(form->tc(), initTime,-rho_init);
+            bc->addDirichlet(form->tm(1),initTime,-m1_init);
+            bc->addDirichlet(form->te(), initTime,-E_init);
+          }
+          break;
+        case 2:
+          // bc->addDirichlet(form->rho(),   leftX,    _exactMap[form->rho()->ID()]);
+          // bc->addDirichlet(form->rho(),   rightX,   _exactMap[form->rho()->ID()]);
+          // bc->addDirichlet(form->rho(),   leftY,    _exactMap[form->rho()->ID()]);
+          // bc->addDirichlet(form->rho(),   rightY,   _exactMap[form->rho()->ID()]);
+          // bc->addDirichlet(form->uhat(1), leftX,    _exactMap[form->uhat(1)->ID()]);
+          // bc->addDirichlet(form->uhat(2), leftX,    _exactMap[form->uhat(2)->ID()]);
+          // bc->addDirichlet(form->uhat(1), rightX,   _exactMap[form->uhat(1)->ID()]);
+          // bc->addDirichlet(form->uhat(2), rightX,   _exactMap[form->uhat(2)->ID()]);
+          // bc->addDirichlet(form->uhat(1), leftY,    _exactMap[form->uhat(1)->ID()]);
+          // bc->addDirichlet(form->uhat(2), leftY,    _exactMap[form->uhat(2)->ID()]);
+          // bc->addDirichlet(form->uhat(1), rightY,   _exactMap[form->uhat(1)->ID()]);
+          // bc->addDirichlet(form->uhat(2), rightY,   _exactMap[form->uhat(2)->ID()]);
+          // bc->addDirichlet(form->T(),     leftX,    _exactMap[form->T()->ID()]);
+          // bc->addDirichlet(form->T(),     rightX,   _exactMap[form->T()->ID()]);
+          // bc->addDirichlet(form->T(),     leftY,    _exactMap[form->T()->ID()]);
+          // bc->addDirichlet(form->T(),     rightY,   _exactMap[form->T()->ID()]);
+          break;
+        case 3:
+          break;
+      }
+    }
+
+    double computeL2Error(SpaceTimeCompressibleFormulationPtr form, SolutionPtr solutionBackground)
+    {
+      FunctionPtr rho_soln, u1_soln, u2_soln, u3_soln, T_soln,
+                  rho_diff, u1_diff, u2_diff, u3_diff, T_diff,
+                  rho_sqr, u1_sqr, u2_sqr, u3_sqr, T_sqr;
+      rho_soln = Function::solution(form->rho(), solutionBackground);
+      u1_soln = Function::solution(form->u(1), solutionBackground);
+      if (form->spaceDim() > 1)
+        u2_soln = Function::solution(form->u(2), solutionBackground);
+      if (form->spaceDim() > 2)
+        u3_soln = Function::solution(form->u(3), solutionBackground);
+      T_soln = Function::solution(form->T(), solutionBackground);
+      rho_diff = rho_soln - _rho_exact;
+      u1_diff = u1_soln - _u1_exact;
+      if (form->spaceDim() > 1)
+        u2_diff = u2_soln - _u2_exact;
+      if (form->spaceDim() > 2)
+        u3_diff = u3_soln - _u3_exact;
+      T_diff = T_soln - _T_exact;
+      rho_sqr = rho_diff*rho_diff;
+      u1_sqr = u1_diff*u1_diff;
+      if (form->spaceDim() > 1)
+        u2_sqr = u2_diff*u2_diff;
+      if (form->spaceDim() > 2)
+        u3_sqr = u3_diff*u3_diff;
+      T_sqr = T_diff*T_diff;
+      double rho_l2, u1_l2, u2_l2 = 0, u3_l2 = 0, T_l2;
+      rho_l2 = rho_sqr->integrate(solutionBackground->mesh(), 5);
+      u1_l2 = u1_sqr->integrate(solutionBackground->mesh(), 5);
+      if (form->spaceDim() > 1)
+        u2_l2 = u2_sqr->integrate(solutionBackground->mesh(), 5);
+      if (form->spaceDim() > 2)
+        u3_l2 = u3_sqr->integrate(solutionBackground->mesh(), 5);
+      T_l2 = T_sqr->integrate(solutionBackground->mesh(), 5);
+      double l2Error = sqrt(rho_l2+u1_l2+u2_l2+u3_l2+T_l2);
+      return l2Error;
+    }
+};
+
+class TrivialCompressible : public AnalyticalCompressibleProblem
+{
+  private:
+  public:
+    TrivialCompressible(bool steady, double Re)
+    {
+      _steady = steady;
+      _gamma = 1.4;
+      double p0 = 1;
+      double rho0 = 1;
+      double u0 = 1;
+      double a0 = sqrt(_gamma*p0/rho0);
+      double M_inf = u0/a0;
+      _Cv = 1./(_gamma*(_gamma-1)*M_inf*M_inf);
+      _Cp = _gamma*_Cv;
+      _R = _Cp-_Cv;
+      _rho_exact = Function::constant(rho0);
+      _u1_exact = Function::constant(u0);
+      _T_exact = Function::constant(p0/(rho0*_R));
+
+      _x0.push_back(0);
+      // _x0.push_back(0);
+      _dimensions.push_back(1);
+      // _dimensions.push_back(1);
+      _elementCounts.push_back(2);
+      // _elementCounts.push_back(2);
+      _tInit = 0.0;
+      _tFinal = 0.5;
+    }
+};
+
+}
